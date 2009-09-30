9bdb78fb9d6cf44e963c9ed9a6b53f353250ae6f
David Tchepak <dave@davesquared.net>
2009-09-30 23:46:24 +1000

    Getting rake build working
    - Added Build/rakefile.rb with compile and test targets
    - Switched solution and projects to target x86 arch.
    - Updated projects to build to Output directory.
    - Treating warnings as errors for all configurations.

---
+++ Build/rakefile.rb
@@ -0,0 +1,31 @@
+require 'rake/clean'
+
+DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v3.5"
+NUNIT_EXE = "../ThirdParty/Nunit-2.5.0.9122/bin/net-2.0/nunit-console-x86.exe"
+SOURCE_PATH = "../Source"
+OUTPUT_PATH = "../Output"
+CONFIG = "Debug"
+ 
+CLEAN.include(OUTPUT_PATH)
+
+task :default => ["clean", "build:all"]
+ 
+namespace :build do
+  
+  task :all => [:compile, :test]
+      
+  desc "Build solutions using MSBuild"
+  task :compile do
+    solutions = FileList["#{SOURCE_PATH}/**/*.sln"]
+    solutions.each do |solution|
+      sh "#{DOT_NET_PATH}/msbuild.exe /p:Configuration=#{CONFIG} #{solution}"
+    end
+  end
+   
+  desc "Runs tests with NUnit"
+  task :test => [:compile] do
+    tests = FileList["#{OUTPUT_PATH}/**/*.Tests.dll"].exclude(/obj\//)
+    sh "#{NUNIT_EXE} #{tests} /nologo /xml=#{OUTPUT_PATH}/TestResults.xml"
+  end
+  
+end
