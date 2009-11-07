08cc321aee44b223604877f715081e90a25389e5
David Tchepak <dave@davesquared.net>
2009-11-07 12:07:07 +1100

    Adding VS2010 Beta2 solution
    - Updated rakefile to only build non-vs2010 solution.
    - Added NSubstitute.2010.sln.
    - csproj files updated.

--- Build/rakefile.rb
+++ Build/rakefile.rb
@@ -16,7 +16,7 @@ namespace :build do
       
   desc "Build solutions using MSBuild"
   task :compile do
-    solutions = FileList["#{SOURCE_PATH}/**/*.sln"]
+    solutions = FileList["#{SOURCE_PATH}/**/*.sln"].exclude(/\.2010\./)
     solutions.each do |solution|
       sh "#{DOT_NET_PATH}/msbuild.exe /p:Configuration=#{CONFIG} #{solution}"
     end
