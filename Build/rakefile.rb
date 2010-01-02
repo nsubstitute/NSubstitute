8b3cdb3d9a2eb8b61ed3b8dbe2f3d95025b52451
David Tchepak <dave@davesquared.net>
2010-01-02 22:17:51 +1100

    Adding acceptance specs to SLN and updating build file
    - Removed "Any CPU" target
    - Added acceptance specs project to VS2008 SLN file.
    - Updated build batch file to pass on arguments to rake
    - Removed :build namespace from rakefile to make it easier to call tasks
    - Updated standard build to exclude acceptance tests (as they might be unimplemented -- need a better solution here)
    - Introduced acceptance tests task to rake file.
    - Can run acceptance tests using "./build acceptance" or "rake acceptance"

--- Build/rakefile.rb
+++ Build/rakefile.rb
@@ -5,27 +5,29 @@ NUNIT_EXE = "../ThirdParty/Nunit-2.5.0.9122/bin/net-2.0/nunit-console-x86.exe"
 SOURCE_PATH = "../Source"
 OUTPUT_PATH = "../Output"
 CONFIG = "Debug"
- 
+
 CLEAN.include(OUTPUT_PATH)
 
-task :default => ["clean", "build:all"]
- 
-namespace :build do
+task :default => ["clean", "all"]
+task :all => [:compile, :test]
   
-  task :all => [:compile, :test]
-      
-  desc "Build solutions using MSBuild"
-  task :compile do
+desc "Build solutions using MSBuild"
+task :compile do
     solutions = FileList["#{SOURCE_PATH}/**/*.sln"].exclude(/\.2010\./)
     solutions.each do |solution|
-      sh "#{DOT_NET_PATH}/msbuild.exe /p:Configuration=#{CONFIG} #{solution}"
-    end
-  end
-   
-  desc "Runs tests with NUnit"
-  task :test => [:compile] do
-    tests = FileList["#{OUTPUT_PATH}/**/*.Specs.dll"].exclude(/obj\//)
-    sh "#{NUNIT_EXE} #{tests} /nologo /xml=#{OUTPUT_PATH}/TestResults.xml"
-  end
-  
+    sh "#{DOT_NET_PATH}/msbuild.exe /p:Configuration=#{CONFIG} #{solution}"
 end
+end
+
+desc "Runs tests with NUnit"
+task :test => [:compile] do
+    tests = FileList["#{OUTPUT_PATH}/**/NSubstitute.Specs.dll"].exclude(/obj\//)
+    sh "#{NUNIT_EXE} #{tests} /nologo /xml=#{OUTPUT_PATH}/TestResults.xml"
+end
+
+desc "Runs acceptance tests with NUnit"
+task :acceptance => [:compile] do
+    acceptance_tests = FileList["#{OUTPUT_PATH}/**/NSubstitute.Acceptance.Specs.dll"].exclude(/obj\//)
+    sh "#{NUNIT_EXE} #{acceptance_tests} /nologo /xml=#{OUTPUT_PATH}/AcceptanceTestResults.xml"
+end
+
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 cd Build
-rake
+rake %*
 cd ..
