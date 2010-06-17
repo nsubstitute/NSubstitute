270c2f32f1f6d10f11486f73f6dc82ce2f37c04d
dtchepak <dave@davesquared.net>
2010-06-17 15:24:41 +1000

    Updating Substitute.For interface
    Changing how multiple types are subbed for.
    Still need to implement.

--- Source/NSubstitute/Substitute.cs
+++ Source/NSubstitute/Substitute.cs
@@ -6,25 +6,31 @@ namespace NSubstitute
     public class Substitute
     {
         private static readonly Type[] NoAdditionalInterfaceTypes = new Type[0];
-        private static readonly object[] NoConstructorArguments = new object[0];
-
-        public static T For<T>() where T : class
-        {
-            return For<T>(NoAdditionalInterfaceTypes, NoConstructorArguments);
-        }
-
-        public static T For<T>(params Type[] additionalInterfaces) where T : class
-        {
-            return For<T>(additionalInterfaces, NoConstructorArguments);
-        }
 
         public static T For<T>(params object[] constructorArguments) where T : class
         {
             return For<T>(NoAdditionalInterfaceTypes, constructorArguments);
-        } 
+        }
 
-        public static T For<T>(Type[] additionalInterfaces, params object[] constructorArguments) where T : class
+        public static T1 For<T1, T2>(params object[] constructorArguments)
+            where T1 : class
+            where T2 : class
         {
+            return For<T1>(new[] { typeof(T2) }, constructorArguments);
+        }
+
+        public static T1 For<T1, T2, T3>(params object[] constructorArguments)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+        {
+            return For<T1>(new[] { typeof(T2), typeof(T3) }, constructorArguments);
+        }
+
+        public static T For<T>(Type[] additionalInterfaces, object[] constructorArguments) where T : class
+        {
+            //This should not be a generic method
+            //Should check to make sure there is not more than 1 class being subbed for.
             var substituteFactory = SubstitutionContext.Current.GetSubstituteFactory();
             return substituteFactory.Create<T>(new Type[0], new object[0]);
         }
