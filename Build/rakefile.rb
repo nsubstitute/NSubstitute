0da33973f6ceedd448226c9cc12de1e552160f8a
dtchepak <dave@davesquared.net>
2010-10-10 23:18:49 +1100

    Adding nupack task to build (from David Burela's work)
    Removed ununsed docgenerator.rb
    Added .nuspec
    Fixed paths in rakefile (possibly changed between Ruby 1.8x and 1.9)
    Added :nupack task
    Add nupack console tool to ThirdParty libraries.

--- Build/rakefile.rb
+++ Build/rakefile.rb
@@ -1,9 +1,11 @@
 require 'rake/clean'
 require File.expand_path('lib/examples_to_code.rb', File.dirname(__FILE__))
 require 'FileUtils'
+require 'date'
 
 DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v3.5"
 NUNIT_EXE = "../ThirdParty/NUnit/bin/net-2.0/nunit-console.exe"
+NUPACK_EXE = "../ThirdParty/NuPack/NuPack.exe"
 SOURCE_PATH = "../Source"
 OUTPUT_PATH = "../Output"
 
@@ -111,12 +113,41 @@ task :package => [:compile, :version_assemblies, :generate_docs] do
 	output_base_path = "#{OUTPUT_PATH}/#{CONFIG}"
 	dll_path = "#{output_base_path}/NSubstitute"
 	deploy_path = "#{output_base_path}/NSubstitute-#{@@build_number}"
-	#mkdir deploy_path unless File.exists? deploy_path
-	cp_r dll_path, deploy_path
+
+    mkdir_p deploy_path
+	cp Dir.glob("#{dll_path}/*.{dll,xml}"), deploy_path
+
 	cp "../README.markdown", "#{deploy_path}/README.txt"
 	cp "../LICENSE.txt", "#{deploy_path}"
 	cp "../CHANGELOG.txt", "#{deploy_path}"
     tidyUpTextFileFromMarkdown("#{deploy_path}/README.txt")
+end
+
+desc "Create NuPack package"
+task :nupack => [:package] do
+	output_base_path = "#{OUTPUT_PATH}/#{CONFIG}"
+	dll_path = "#{output_base_path}/NSubstitute"
+	deploy_path = "#{output_base_path}/NSubstitute-#{@@build_number}"
+	nupack_path = "#{output_base_path}/nupack/#{@@build_number}"
+	nupack_lib_path = "#{output_base_path}/nupack/#{@@build_number}/lib/35"
+
+    #Ensure nupack path exists
+    mkdir_p nupack_lib_path
+
+    #Copy binaries into lib path
+    cp Dir.glob("#{dll_path}/*.{dll,xml}"), nupack_lib_path
+
+    #Copy nuspec and *.txt docs into package root
+    cp Dir.glob("#{deploy_path}/*.txt"), nupack_path
+    cp "NSubstitute.nuspec", nupack_path
+    updateNuspec("#{nupack_path}/NSubstitute.nuspec")
+
+    #Build package
+    full_path_to_nupack_exe = File.expand_path(NUPACK_EXE, File.dirname(__FILE__))
+    nuspec = File.expand_path("#{nupack_path}/NSubstitute.nuspec", File.dirname(__FILE__))
+    FileUtils.cd "#{output_base_path}/nupack" do
+        sh "#{full_path_to_nupack_exe} #{nuspec}"
+    end
 end
 
 def tidyUpTextFileFromMarkdown(file)
@@ -134,4 +165,12 @@ def stripHtmlComments(text)
 
     text[indexOfStart..(indexOfEnd+endComment.length-1)] = ""
     return stripHtmlComments(text)
+end
+
+def updateNuspec(file)
+    text = File.read(file)
+    modified_date = DateTime.now.rfc3339
+    text.gsub! /<version>.*?<\/version>/, "<version>#{@@build_number}</version>"
+    text.gsub! /<modified>.*?<\/modified>/, "<modified>#{modified_date}</modified>"
+    File.open(file, 'w') { |f| f.write(text) }
 end
