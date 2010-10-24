4852fb62a395b3a401bd2a4151d97b4a3371c81d
dtchepak <dave@davesquared.net>
2010-10-24 22:41:29 +1100

    Removing unused generate_docs method in examples_to_code.rb

--- Build/rakelib/lib/examples_to_code.rb
+++ Build/rakelib/lib/examples_to_code.rb
@@ -4,9 +4,9 @@ class ExamplesToCode
     def self.create()
         file_reader_writer = FileReaderWriter.new
         converter = FileConverter.new(file_reader_writer, TextToCodeConverter.new(CodeExtractor.new))
-        ExamplesToCode.new(FileFinder.new, converter) 
+        ExamplesToCode.new(FileFinder.new, converter)
     end
-    
+
 	def initialize(file_finder, converter)
         @file_finder = file_finder
         @converter = converter
@@ -23,24 +23,3 @@ class ExamplesToCode
     end
 end
 
-def generate_docs
-	files_for_generation = FileList.new ["#{SOURCE_PATH}/Docs/*.template"]
-
-	Dir.glob(files_for_generation).each do |original_file| 
-	  new_file = DOCS_PATH + "/" + File.basename(original_file, '.template')
-	  is_html = new_file =~ /\.html$/
-
-	  cp original_file, new_file
-	  content = File.new(new_file,'r').read	
-	  
-	  replacement_tags = content.scan(/\{CODE:.+\}/)
-	  
-	  replacement_tags.each do |tag|
-		replacement_content = get_replacement(tag)
-		replacement_content = to_html(replacement_content) if (is_html)
-		content.gsub!(tag.to_s, replacement_content)
-	  end
-
-	  File.open(new_file, 'w') { |fw| fw.write(content) }
-	end
-end
