b1d0352c6b0d5b751bc08799a950c13018e5f6f1
dtchepak <dave@davesquared.net>
2010-10-26 23:00:58 +1100

    Testing some more NotASubstituteException cases

--- Source/NSubstitute.Acceptance.Specs/NotASubstituteExceptions.cs
+++ Source/NSubstitute.Acceptance.Specs/NotASubstituteExceptions.cs
@@ -1,3 +1,4 @@
+using System;
 using NSubstitute.Exceptions;
 using NUnit.Framework;
 
@@ -5,12 +6,19 @@ namespace NSubstitute.Acceptance.Specs
 {
     public class NotASubstituteExceptions
     {
+        object _notASub;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _notASub = new object();
+        }
+
         [Test]
         public void Should_describe_how_exception_can_occur()
         {
-            var notASub = new object();
             Assert.That(
-                () => notASub.Received(),
+                () => _notASub.Received(),
                 Throws
                     .TypeOf<NotASubstituteException>()
                 .And
@@ -18,6 +26,24 @@ namespace NSubstitute.Acceptance.Specs
                         "NSubstitute extension methods like .Received() can only be called on objects created using Substitute.For<T>() and related methods."
                     )
             );
+        }
+
+        [Test]
+        public void Calling_received_on_a_non_sub()
+        {
+            AssertThrowsNotASubstituteException(() => _notASub.Received());
+        }
+
+        [Test]
+        public void Calling_when_on_a_non_sub()
+        {
+            AssertThrowsNotASubstituteException(() => _notASub.When(x => x.GetHashCode()));
+        }
+
+
+        private static void AssertThrowsNotASubstituteException(Action action)
+        {
+            Assert.That(() => action(), Throws.TypeOf<NotASubstituteException>());
         }
     }
 }
