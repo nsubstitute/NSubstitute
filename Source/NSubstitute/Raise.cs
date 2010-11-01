341d9cc890197f3abf3bc8c9402e7af1ff3ffcfc
dtchepak <dave@davesquared.net>
2010-11-01 22:48:38 +1100

    Adding xmldoc for Raise class methods.

--- Source/NSubstitute/Raise.cs
+++ Source/NSubstitute/Raise.cs
@@ -8,30 +8,44 @@ namespace NSubstitute
 {
     public static class Raise
     {
+        /// <summary>
+        /// Raise an event for an <c>EventHandler&lt;TEventArgs&gt;</c> event with the provided <paramref name="sender"/> and <paramref name="eventArgs"/>.
+        /// </summary>
         public static EventHandlerWrapper<TEventArgs> EventWith<TEventArgs>(object sender, TEventArgs eventArgs) where TEventArgs : EventArgs
         {
             return new EventHandlerWrapper<TEventArgs>(sender, eventArgs);
         }
 
+        /// <summary>
+        /// Raise an event for an <c>EventHandler&lt;TEventArgs&gt;</c> event with the substitute as the sender and the provided <paramref name="eventArgs" />.
+        /// </summary>
         public static EventHandlerWrapper<TEventArgs> EventWith<TEventArgs>(TEventArgs eventArgs) where TEventArgs : EventArgs
         {
             return new EventHandlerWrapper<TEventArgs>(eventArgs);
         }
 
+        /// <summary>
+        /// Raise an event for an <c>EventHandler&lt;EventArgsT&gt;</c> event with the substitute as the sender
+        /// and with a default instance of <typeparamref name="TEventArgs" />.
+        /// </summary>
         public static EventHandlerWrapper<TEventArgs> EventWith<TEventArgs>() where TEventArgs : EventArgs
         {
             return new EventHandlerWrapper<TEventArgs>();
         }
 
         /// <summary>
-        /// Raise an event for an <see>EventHandler</see> event using empty <see>EventArgs</see>.
+        /// Raise an event for an <c>EventHandler</c> or <c>EventHandler&lt;EventArgs&gt;</c> event with the substitute
+        /// as the sender and with empty <c>EventArgs</c>.
         /// </summary>
-        /// <returns></returns>
         public static EventHandlerWrapper<EventArgs> Event()
         {
             return new EventHandlerWrapper<EventArgs>();
         }
 
+        /// <summary>
+        /// Raise an event of type <typeparamref name="THandler" /> with the provided arguments. If no arguments are provided
+        /// NSubstitute will try and provide reasonble defaults.
+        /// </summary>
         public static DelegateEventWrapper<THandler> Event<THandler>(params object[] arguments)
         {
             return new DelegateEventWrapper<THandler>(arguments);
@@ -40,7 +54,7 @@ namespace NSubstitute
 
     public class EventHandlerWrapper<TEventArgs> where TEventArgs : EventArgs
     {
-        object _sender;
+        readonly object _sender;
         readonly EventArgs _eventArgs;
 
         public EventHandlerWrapper()
@@ -82,8 +96,6 @@ namespace NSubstitute
                                                       sender = call.Target();
                                                   if (eventArgs == null)
                                                       eventArgs = GetDefaultForEventArgType(typeof(TEventArgs));
-
-
                                                   return new[] { sender, eventArgs };
                                               });
         }
