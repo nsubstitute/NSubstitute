29dee9845f2718d90767661c628e1d7a41277613
David Tchepak <dave@davesquared.net>
2011-02-15 22:57:17 +1100

    Minor cleanup of DisposeWithThreadLocal fixture.
    Couldn't come up with a way to get a failing unit test for this. Have to manually look at test runner output instead.

--- Source/NSubstitute.Acceptance.Specs/FieldReports/DisposeWithThreadLocal.cs
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/DisposeWithThreadLocal.cs
@@ -8,34 +8,21 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
         [Test]
         public void DisposeSubstituteAndPerformGC()
         {
-            using (var s = Substitute.For<DisposableClass>())
-            { }
-
+            using (var s = Substitute.For<DisposableClass>()) { } 
             GC.Collect();
             GC.WaitForPendingFinalizers();
+
+            //Exception thrown on background thread. Can view this from output of test runner.
         }
 
         public class DisposableClass : IDisposable
         {
             bool disposed = false;
-
             ~DisposableClass() { Dispose(false); }
-
-            public virtual void Dispose()
-            {
-                Dispose(true);
-                GC.SuppressFinalize(this);
-            }
-
+            public virtual void Dispose() { Dispose(true); }
             protected virtual void Dispose(bool disposing)
             {
-                if (!disposed)
-                {
-                    if (disposing)
-                    { // Dispose something }
-                        disposed = true;
-                    }
-                }
+                if (!disposed) { if (disposing) { disposed = true; } }
             }
         }
     }
