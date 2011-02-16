3e02085d190bbee298d38b2809d60dbca0c8ab29
David Tchepak <dave@davesquared.net>
2011-02-16 14:28:13 +1100

    Adding more examples to CustomArgumentMatchersWhichDoNotHandleNulls acceptance test.

--- Source/NSubstitute.Acceptance.Specs/FieldReports/CustomArgumentMatchersWhichDoNotHandleNulls.cs
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/CustomArgumentMatchersWhichDoNotHandleNulls.cs
@@ -8,20 +8,44 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
 
         [Test]
         [Pending]
-        public void Condition_that_requires_object_reference()
+        public void Single_condition_that_does_not_handle_nulls()
         {
             var request = Substitute.For<IRequest>();
             request.Get(Arg.Is<string>(x => x.Contains("greeting"))).Returns("hello world");
-            request.Get(Arg.Is<string>(x => string.IsNullOrEmpty(x))).Returns("?");
 
             Assert.That(request.Get("greeting"), Is.EqualTo("hello world"));
-            Assert.That(request.Get(""), Is.EqualTo("?"));
-            Assert.That(request.Get(null), Is.EqualTo("?"));
+            Assert.That(request.Get(null), Is.EqualTo(""));
         }
 
         [Test]
         [Pending]
-        public void Negated_condition_that_requires_object_reference()
+        public void Single_negated_condition_that_does_not_handle_nulls()
+        {
+            var request = Substitute.For<IRequest>();
+            request.Get(Arg.Is<string>(x => !x.Contains("please"))).Returns("ask nicely");
+
+            Assert.That(request.Get("greeting"), Is.EqualTo("ask nicely"));
+            Assert.That(request.Get(null), Is.EqualTo("ask nicely"));
+            Assert.Fail("Should Get(null) return 'ask nicely' or string.empty here?");
+        }
+
+        [Test]
+        [Pending]
+        public void Multiple_conditions_that_require_an_object_reference()
+        {
+            var request = Substitute.For<IRequest>();
+            request.Get(Arg.Is<string>(x => x.Contains("greeting"))).Returns("hello world");
+            request.Get(Arg.Is<string>(x => x.Length < 5)).Returns("?");
+
+            Assert.That(request.Get("greeting"), Is.EqualTo("hello world"));
+            Assert.That(request.Get(""), Is.EqualTo("?"));
+            Assert.That(request.Get(null), Is.EqualTo(""));
+            Assert.Fail("Should Get(null) return '?' or string.empty here?");
+        }
+
+        [Test]
+        [Pending]
+        public void Multiple_negated_conditions_that_requires_an_object_reference()
         {
             var request = Substitute.For<IRequest>();
             request.Get(Arg.Is<string>(x => !x.StartsWith("please"))).Returns("ask nicely");
