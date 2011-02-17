7c8fd9a6109702b9768f2eae82cd8e448bb0fcf7
David Tchepak <dave@davesquared.net>
2011-02-18 09:59:49 +1100

    Added example of matchers which require an array index availbale to CustomArgumentMatchersWhichDoNotHandleNulls acceptance test.

--- Source/NSubstitute.Acceptance.Specs/FieldReports/CustomArgumentMatchersWhichDoNotHandleNulls.cs
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/CustomArgumentMatchersWhichDoNotHandleNulls.cs
@@ -4,7 +4,10 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
 {
     public class CustomArgumentMatchersWhichDoNotHandleNulls
     {
-        public interface IRequest { string Get(string url); }
+        public interface IRequest { 
+            string Get(string url);
+            string[] GetMultiple(string[] url);
+        }
 
         [Test]
         [Pending]
@@ -25,8 +28,8 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
             request.Get(Arg.Is<string>(x => !x.Contains("please"))).Returns("ask nicely");
 
             Assert.That(request.Get("greeting"), Is.EqualTo("ask nicely"));
-            Assert.That(request.Get(null), Is.EqualTo("ask nicely"));
-            Assert.Fail("Should Get(null) return 'ask nicely' or string.empty here?");
+            Assert.That(request.Get(null), Is.EqualTo(""));
+            //Assert.Fail("Should Get(null) return 'ask nicely' or string.empty here?");
         }
 
         [Test]
@@ -40,7 +43,7 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
             Assert.That(request.Get("greeting"), Is.EqualTo("hello world"));
             Assert.That(request.Get(""), Is.EqualTo("?"));
             Assert.That(request.Get(null), Is.EqualTo(""));
-            Assert.Fail("Should Get(null) return '?' or string.empty here?");
+            //Assert.Fail("Should Get(null) return '?' or string.empty here?");
         }
 
         [Test]
@@ -58,6 +61,17 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
 
         [Test]
         [Pending]
+        public void Condition_that_requires_object_ref_and_condition_that_covers_null()
+        {
+            var request = Substitute.For<IRequest>();
+            request.Get(Arg.Is<string>(x => x.Contains("greeting"))).Returns("hello world");
+            request.Get(null).Returns("?");
+
+            Assert.That(request.Get(null), Is.EqualTo("?"));
+        }
+
+        [Test]
+        [Pending]
         public void Multiple_conditions_with_multiple_returns()
         {
             var request = Substitute.For<IRequest>();
@@ -66,6 +80,18 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
 
             Assert.That(request.Get(null), Is.EqualTo("huh?"));
             Assert.That(request.Get(null), Is.EqualTo("what?"));
+        }
+
+        [Test]
+        [Pending]
+        public void Multiple_conditions_where_one_requires_an_array_index_available()
+        {
+            var request = Substitute.For<IRequest>();
+            request.GetMultiple(Arg.Is<string[]>(x => x[0] == "greeting")).Returns(new[] { "hello", "bye" });
+            request.GetMultiple(new string[0]).Returns(new [] {"?"});
+
+            Assert.That(request.GetMultiple(new [] {"greeting"}), Is.EqualTo(new[] {"hello", "bye"}));
+            Assert.That(request.GetMultiple(new string[0]), Is.EqualTo(new[] { "?" }));
         }
     }
 }
