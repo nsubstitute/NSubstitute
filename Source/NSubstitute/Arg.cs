5b0c165efe8b32ee3d2bafb83992bc8f588eb391
David Tchepak <dave@davesquared.net>
2011-08-09 11:16:10 +1000

    Moving start of Arg.Invoke feature to its own branch
    Hooks for Arg.Invoke are in master branch with no implementation.
    Removing from master so it won't cause confusion.

--- Source/NSubstitute/Arg.cs
+++ Source/NSubstitute/Arg.cs
@@ -22,36 +22,6 @@ namespace NSubstitute
             return EnqueueArgumentSpec<T>(new ArgumentMatchesSpecification<T>(predicate));
         }
 
-        public static Action Invoke()
-        {
-            return EnqueueArgumentSpec<Action>(new ArgumentIsAnythingSpecification(typeof(Action)));
-        }
-
-        public static Action<T> Invoke<T>(T arg)
-        {
-            return EnqueueArgumentSpec<Action<T>>(new ArgumentIsAnythingSpecification(typeof(Action<T>)));
-        }
-
-        public static Action<T1, T2> Invoke<T1, T2>(T1 arg1, T2 arg2)
-        {
-            return EnqueueArgumentSpec<Action<T1, T2>>(new ArgumentIsAnythingSpecification(typeof(Action<T1, T2>)));
-        }
-
-        public static Action<T1, T2, T3> Invoke<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3)
-        {
-            return EnqueueArgumentSpec<Action<T1, T2, T3>>(new ArgumentIsAnythingSpecification(typeof(Action<T1, T2, T3>)));
-        }
-
-        public static Action<T1, T2, T3, T4> Invoke<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
-        {
-            return EnqueueArgumentSpec<Action<T1, T2, T3, T4>>(new ArgumentIsAnythingSpecification(typeof(Action<T1, T2, T3, T4>)));
-        }
-
-        public static TDelegate InvokeDelegate<TDelegate>(params object[] arguments)
-        {
-            return EnqueueArgumentSpec<TDelegate>(new ArgumentIsAnythingSpecification(typeof(TDelegate)));
-        }
-
         private static T EnqueueArgumentSpec<T>(IArgumentSpecification argumentSpecification)
         {
             SubstitutionContext.Current.EnqueueArgumentSpecification(argumentSpecification);
