82edc3eb849ee0ff3e9028a194c74764f89c0e2b
David Tchepak <dave@davesquared.net>
2012-08-15 10:31:37 +1000

    Adding repro for issue #83.

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue83_MethodsWithGenericStructConstraint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue83_MethodsWithGenericStructConstraint
+    {
+        public interface IService { T Get<T>(T arg) where T : struct; }
+
+        [Test]
+        [Pending]
+        public void TestGenericCalls()
+        {
+            var id = Guid.NewGuid();
+            var service = Substitute.For<IService>();
+            service.Get(id);
+            service.Received().Get(id);
+        }
+    }
+}
