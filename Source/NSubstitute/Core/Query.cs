38f8410f7e661ae68533b4104ee02889def7369c
David Tchepak <dave@davesquared.net>
2012-10-09 16:59:55 +1100

    Adding CallQueryRoute and AddCallToQueryResultHandler (not wired up).
    Query represents a query in progress.

---
+++ Source/NSubstitute/Core/Query.cs
@@ -0,0 +1,10 @@
+﻿namespace NSubstitute.Core
+{
+    public class Query
+    {
+        public virtual void Add(object target, ICallSpecification callSpecification)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Source/NSubstitute/Routing/Handlers/AddCallToQueryResultHandler.cs
@@ -0,0 +1,24 @@
+﻿using NSubstitute.Core;
+
+namespace NSubstitute.Routing.Handlers
+{
+    public class AddCallToQueryResultHandler : ICallHandler
+    {
+        private readonly Query _query;
+        private readonly ICallSpecificationFactory _callSpecificationFactory;
+
+        public AddCallToQueryResultHandler(Query query, ICallSpecificationFactory callSpecificationFactory)
+        {
+            _query = query;
+            _callSpecificationFactory = callSpecificationFactory;
+        }
+
+        public RouteAction Handle(ICall call)
+        {
+            var target = call.Target();
+            var callSpec = _callSpecificationFactory.CreateFrom(call, MatchArgs.AsSpecifiedInCall);
+            _query.Add(target, callSpec);
+            return RouteAction.Continue();
+        }
+    }
+}
