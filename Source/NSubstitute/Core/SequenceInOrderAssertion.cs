f1ce3405a2a02514af934554fb9f00c39363caf0
David Tchepak <dave@davesquared.net>
2012-11-18 19:07:47 +1100

    Adding formatted arguments to SequenceInOrderAssertion exception message

--- Source/NSubstitute/Core/SequenceInOrderAssertion.cs
+++ Source/NSubstitute/Core/SequenceInOrderAssertion.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using NSubstitute.Core.Arguments;
 using NSubstitute.Exceptions;
 
 namespace NSubstitute.Core
@@ -7,10 +9,12 @@ namespace NSubstitute.Core
     public class SequenceInOrderAssertion
     {
         private readonly CallFormatter _callFormatter;
+        private readonly ArgumentFormatter _argumentFormatter;
 
         public SequenceInOrderAssertion()
         {
             _callFormatter = new CallFormatter();
+            _argumentFormatter = new ArgumentFormatter();
         }
 
         public void Assert(IQueryResults queryResult)
@@ -51,13 +55,26 @@ namespace NSubstitute.Core
             var formattedQuery = string.Join(callDelimiter,
                                              querySpec.Select(x => x.CallSpecification.ToString()).ToArray());
             var formattedCalls = string.Join(callDelimiter,
-                                             matchingCallsInOrder.Select(x => _callFormatter.Format(x.GetMethodInfo(), new [] {""})).ToArray());
+                                             matchingCallsInOrder.Select(x => 
+                                                 _callFormatter.Format(x.GetMethodInfo(), FormatArgs(x.GetArguments()))).ToArray());
             return string.Format("\nExpected to receive these calls in order:{0}{1}\n" + 
                                  "Actually received matching calls in this order:{0}{2}\n\n{3}",
                                  callDelimiter,
                                  formattedQuery,
                                  formattedCalls,
                                  "*** Note: calls to property getters are not considered part of the query. ***");
+        }
+
+        private bool IsAcrossMultipleTargets(CallSpecAndTarget[] querySpec)
+        {
+            if (!querySpec.Any()) return false;
+            var firstTarget = querySpec.First().Target;
+            return querySpec.Any(x => !ReferenceEquals(firstTarget, x.Target));
+        }
+
+        private IEnumerable<string> FormatArgs(object[] arguments)
+        {
+            return arguments.Select(x => _argumentFormatter.Format(x, false));
         }
 
         private bool Matches(ICall call, CallSpecAndTarget specAndTarget)
