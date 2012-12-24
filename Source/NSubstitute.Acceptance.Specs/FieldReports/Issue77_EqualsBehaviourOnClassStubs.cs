e03fe78a249c8961a860547da3faa96e4bc164c1
Robert Moore <me@robdmoore.id.au>
2012-12-24 08:11:21 +0800

    Added test case from GH-77

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue77_EqualsBehaviourOnClassStubs.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue77_EqualsBehaviourOnClassSubs
+    {
+        [Test]
+        public void Equals_should_work_as_expected_for_class_substitutes_that_overwrites_it()
+        {
+            var firstSub = Substitute.For<AClassThatOverwritesEquals>();
+            var secondSub = Substitute.For<AClassThatOverwritesEquals>();
+
+            Assert.That(firstSub.Equals(firstSub), Is.True);
+            Assert.That(firstSub.Equals(secondSub), Is.Not.True);
+        }
+
+        public class AClassThatOverwritesEquals
+        {
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return 0;
+            }
+        }
+
+        [Test]
+        public void ArgumentMatchers_should_work_with_substituted_equals()
+        {
+            var firstSub = Substitute.For<AClassThatOverwritesEquals>();
+            firstSub.Equals(Arg.Is<object>(x => ReferenceEquals(x, firstSub))).Returns(true);
+            var secondSub = Substitute.For<AClassThatOverwritesEquals>();
+            secondSub.Equals(Arg.Is<object>(x => ReferenceEquals(x, secondSub))).Returns(true);
+
+            var service = Substitute.For<IService>();
+            service.AMethod(firstSub).Returns(1);
+            service.AMethod(secondSub).Returns(2);
+
+            Assert.That(service.AMethod(firstSub), Is.EqualTo(1));
+            Assert.That(service.AMethod(secondSub), Is.EqualTo(2));
+        }
+
+        public interface IService
+        {
+            int AMethod(AClassThatOverwritesEquals aClassThatOverwritesEquals);
+        }
+    }
+}
