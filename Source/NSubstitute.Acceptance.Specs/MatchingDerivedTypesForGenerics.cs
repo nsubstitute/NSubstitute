51ee42e6c3aa720e6b2e51b9f0ba05fae768b445
Iain Ballard <iain.ballard@7digital.com>
2013-01-10 13:19:18 +0000

    Failing test for matching calls to generic methods with derived types

---
+++ Source/NSubstitute.Acceptance.Specs/MatchingDerivedTypesForGenerics.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs
+{
+	[TestFixture]
+	public class MatchingDerivedTypesForGenerics
+	{
+		IGenMethod _sub;
+
+		[SetUp]
+		public void Setup()
+		{
+			_sub = Substitute.For<IGenMethod>();
+		}
+
+
+		[Test]
+		public void Calls_to_generic_types_with_derived_parameters_should_be_matched ()
+		{
+			_sub.Call(new GMParam1());
+			_sub.Call(new GMParam2());
+
+			_sub.Received(2).Call(Arg.Any<IGMParam>());
+		}
+
+
+		public interface IGenMethod
+		{
+			void Call<T>(T param) where T : IGMParam;
+		}
+		public interface IGMParam { }
+		public class GMParam1 : IGMParam { }
+		public class GMParam2 : IGMParam { }
+	}
+
+}
