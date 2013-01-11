03a0a627946f3b07f90d747e6512cd19c0cd2cd8
Iain Ballard <iain.ballard@7digital.com>
2013-01-11 09:33:18 +0000

    Test demonstrating an edge case of generic interfaces

--- Source/NSubstitute.Acceptance.Specs/MatchingDerivedTypesForGenerics.cs
+++ Source/NSubstitute.Acceptance.Specs/MatchingDerivedTypesForGenerics.cs
@@ -31,4 +31,33 @@ namespace NSubstitute.Acceptance.Specs
 		public class GMParam2 : IGMParam { }
 	}
 
+	[TestFixture]
+	public class Calls_to_members_on_generic_interface
+	{
+		[Test]
+		public void Calls_to_members_on_generic_interfaces_match_based_on_type_compatibility()
+		{
+			var sub = Substitute.For<IGenInterface<IGMParam>>();
+			sub.Call(new GMParam1());
+			sub.Call(new GMParam2());
+			sub.Call(new GMParam3());
+
+			sub.Received(2).Call(Arg.Any<IGMParam>());
+			sub.Received(1).Call(Arg.Any<GMParam3>());
+		}
+
+		public interface IGenInterface<T>
+		{
+			void Call(T param);
+			void Call(GMParam3 param);
+		}
+
+		public interface IGMParam { }
+		public class GMParam1 : IGMParam { }
+		public class GMParam2 : IGMParam { }
+		public class GMParam3 { }
+
+	}
+
+
 }
