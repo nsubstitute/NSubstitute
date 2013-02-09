53ebc1678d094b7b01fb1f9a9b4131805c327dd5
Eric Winkler <eric@winkler.net.au>
2013-02-09 13:28:27 +0800

    Renamed IEngine -> ICar to better suit method naming analogy

--- Source/NSubstitute.Acceptance.Specs/ReceivedCalls.cs
+++ Source/NSubstitute.Acceptance.Specs/ReceivedCalls.cs
@@ -8,100 +8,100 @@ namespace NSubstitute.Acceptance.Specs
     [TestFixture]
     public class ReceivedCalls
     {
-        private IEngine _engine;
+        private ICar _car;
         const int Rpm = 7000;
 
         [SetUp]
         public void SetUp()
         {
-            _engine = Substitute.For<IEngine>();
+            _car = Substitute.For<ICar>();
         }
 
         [Test]
         public void Check_when_call_was_received()
         {
-            _engine.Rev();
+            _car.Rev();
 
-            _engine.Received().Rev();
+            _car.Received().Rev();
         }
 
         [Test]
         public void Throw_when_expected_call_was_not_received()
         {
             Assert.Throws<ReceivedCallsException>(() =>
-                    _engine.Received().Idle()
+                    _car.Received().Idle()
                 );
         }
 
         [Test]
         public void Check_call_was_received_with_expected_argument()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            _engine.Received().RevAt(Rpm);
+            _car.Received().RevAt(Rpm);
         }
 
         [Test]
         public void Throw_when_expected_call_was_received_with_different_argument()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
             Assert.Throws<ReceivedCallsException>(() =>
-                    _engine.Received().RevAt(Rpm + 2)
+                    _car.Received().RevAt(Rpm + 2)
                 );
         }
 
         [Test]
         public void Check_that_a_call_was_not_received()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            _engine.DidNotReceive().RevAt(Rpm + 2);
+            _car.DidNotReceive().RevAt(Rpm + 2);
         }
 
         [Test]
         public void Throw_when_a_call_was_not_expected_to_be_received()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.DidNotReceive().RevAt(Rpm));
+            Assert.Throws<ReceivedCallsException>(() => _car.DidNotReceive().RevAt(Rpm));
         }
 
         [Test]
         public void Check_call_received_with_any_arguments()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            _engine.ReceivedWithAnyArgs().RevAt(Rpm + 100);
+            _car.ReceivedWithAnyArgs().RevAt(Rpm + 100);
         }
 
         [Test]
         public void Throw_when_call_was_expected_with_any_arguments()
         {
-            Assert.Throws<ReceivedCallsException>(() => _engine.ReceivedWithAnyArgs().FillPetrolTankTo(10));
+            Assert.Throws<ReceivedCallsException>(() => _car.ReceivedWithAnyArgs().FillPetrolTankTo(10));
         }
 
         [Test]
         public void Check_call_was_not_received_with_any_combination_of_arguments()
         {
-            _engine.DidNotReceiveWithAnyArgs().FillPetrolTankTo(10);
+            _car.DidNotReceiveWithAnyArgs().FillPetrolTankTo(10);
         }
 
         [Test]
         public void Throw_when_call_was_not_expected_to_be_received_with_any_combination_of_arguments()
         {
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.DidNotReceiveWithAnyArgs().RevAt(0));
+            Assert.Throws<ReceivedCallsException>(() => _car.DidNotReceiveWithAnyArgs().RevAt(0));
         }
 
         [Test]
         public void Get_all_received_calls()
         {
-            _engine.Rev();
-            _engine.RevAt(Rpm);
+            _car.Rev();
+            _car.RevAt(Rpm);
 
-            var calls = _engine.ReceivedCalls();
+            var calls = _car.ReceivedCalls();
             var callNames = calls.Select(x => x.GetMethodInfo().Name);
             Assert.That(callNames, Has.Member("Rev"));
             Assert.That(callNames, Has.Member("RevAt"));
@@ -121,115 +121,115 @@ namespace NSubstitute.Acceptance.Specs
         [Test]
         public void Should_receive_call_even_when_call_is_stubbed_to_throw_an_exception()
         {
-            _engine.GetCapacityInLitres().Returns(x => { throw new InvalidOperationException(); });
+            _car.GetCapacityInLitres().Returns(x => { throw new InvalidOperationException(); });
 
             var exceptionThrown = false;
-            try { _engine.GetCapacityInLitres(); }
+            try { _car.GetCapacityInLitres(); }
             catch { exceptionThrown = true; }
 
-            _engine.Received().GetCapacityInLitres();
+            _car.Received().GetCapacityInLitres();
             Assert.That(exceptionThrown, "An exception should have been thrown for this to actually test whether calls that throw exceptions are received.");
         }
 
         [Test]
         public void Should_receive_call_when_a_callback_for_call_throws_an_exception()
         {
-            _engine.When(x => x.Rev()).Do(x => { throw new InvalidOperationException(); });
+            _car.When(x => x.Rev()).Do(x => { throw new InvalidOperationException(); });
 
             var exceptionThrown = false;
-            try { _engine.Rev(); }
+            try { _car.Rev(); }
             catch { exceptionThrown = true; }
 
-            _engine.Received().Rev();
+            _car.Received().Rev();
             Assert.That(exceptionThrown, "An exception should have been thrown for this to actually test whether calls that throw exceptions are received.");
         }
 
         [Test]
         public void Check_when_call_was_received_repeatedly()
         {
-            _engine.Rev();
-            _engine.Rev();
-            _engine.Rev();
+            _car.Rev();
+            _car.Rev();
+            _car.Rev();
 
-            _engine.Received(3).Rev();
+            _car.Received(3).Rev();
         }
 
         [Test]
         public void Check_call_was_not_received_by_making_sure_it_was_called_zero_times()
         {
-            _engine.Received(0).Rev();
+            _car.Received(0).Rev();
         }
 
         [Test]
         public void Throw_when_expected_call_was_not_received_enough_times()
         {
-            _engine.Idle();
-            _engine.Idle();
-            _engine.Idle();
-            _engine.Idle();
+            _car.Idle();
+            _car.Idle();
+            _car.Idle();
+            _car.Idle();
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.Received(5).Idle());
+            Assert.Throws<ReceivedCallsException>(() => _car.Received(5).Idle());
         }
 
         [Test]
         public void Throw_when_expected_call_was_received_too_many_times()
         {
-            _engine.Idle();
-            _engine.Idle();
-            _engine.Idle();
+            _car.Idle();
+            _car.Idle();
+            _car.Idle();
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.Received(2).Idle());
+            Assert.Throws<ReceivedCallsException>(() => _car.Received(2).Idle());
         }
 
         [Test]
         public void Throw_when_expected_call_was_not_received_exactly_zero_times()
         {
-            _engine.Idle();
-            Assert.Throws<ReceivedCallsException>(() => _engine.Received(0).Idle());
+            _car.Idle();
+            Assert.Throws<ReceivedCallsException>(() => _car.Received(0).Idle());
         }
 
         [Test]
         public void Check_call_was_received_a_specifc_number_of_times_with_expected_argument()
         {
             const int differentRpm = Rpm + 2134;
-            _engine.RevAt(Rpm);
-            _engine.RevAt(differentRpm);
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
+            _car.RevAt(differentRpm);
+            _car.RevAt(Rpm);
 
-            _engine.Received(2).RevAt(Rpm);
+            _car.Received(2).RevAt(Rpm);
         }
 
         [Test]
         public void Throw_when_expected_call_was_not_received_a_specific_number_of_times_with_expected_argument()
         {
-            _engine.RevAt(Rpm);
-            _engine.RevAt(Rpm);
+            _car.RevAt(Rpm);
+            _car.RevAt(Rpm);
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.Received(2).RevAt(Rpm + 2));
+            Assert.Throws<ReceivedCallsException>(() => _car.Received(2).RevAt(Rpm + 2));
         }
 
         [Test]
         public void Check_call_received_a_specific_number_of_times_with_any_arguments()
         {
-            _engine.RevAt(1);
-            _engine.RevAt(2);
-            _engine.RevAt(3);
+            _car.RevAt(1);
+            _car.RevAt(2);
+            _car.RevAt(3);
 
-            _engine.ReceivedWithAnyArgs(3).RevAt(Rpm + 100);
+            _car.ReceivedWithAnyArgs(3).RevAt(Rpm + 100);
         }
 
         [Test]
         public void Throw_when_call_was_not_received_a_specific_number_of_times_with_any_arguments()
         {
-            _engine.RevAt(1);
-            _engine.RevAt(2);
-            _engine.RevAt(3);
-            _engine.RevAt(4);
+            _car.RevAt(1);
+            _car.RevAt(2);
+            _car.RevAt(3);
+            _car.RevAt(4);
 
-            Assert.Throws<ReceivedCallsException>(() => _engine.ReceivedWithAnyArgs(2).RevAt(0));
+            Assert.Throws<ReceivedCallsException>(() => _car.ReceivedWithAnyArgs(2).RevAt(0));
         }
 
-        public interface IEngine
+        public interface ICar
         {
             void Start();
             void Rev();
