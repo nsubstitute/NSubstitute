9ebc8b19810c6e5d7753f68ec8bfc50618a8c8cc
Robert Moore <me@robdmoore.id.au>
2013-10-23 22:13:10 +0800

    Adding breaking test for #118 (an issue after adding a public static method to a concrete class that is being mocked)

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue118_ConcreteClassWithPublicStaticMethod.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    class Issue118_ConcreteClassWithPublicStaticMethod
+    {
+        [Test]
+        public void Interface_returning_concrete_without_static_member_should_return_substitute_by_default()
+        {
+            var sub = Substitute.For<IInterfaceReturningConcreteWithoutPublicStaticMethod>();
+
+            var returnedClass = sub.AMethod();
+
+            Assert.That(returnedClass, Is.InstanceOf<ConcreteWithoutPublicStaticMethod>());
+        }
+
+        [Test]
+        public void Interface_returning_concrete_without_static_member_should_return_object_if_instructed_to()
+        {
+            var sub = Substitute.For<IInterfaceReturningConcreteWithoutPublicStaticMethod>();
+            var obj = new ConcreteWithoutPublicStaticMethod();
+            sub.AMethod().Returns(obj);
+
+            var returnedClass = sub.AMethod();
+
+            Assert.That(returnedClass, Is.SameAs(obj));
+        }
+
+        [Test]
+        public void Interface_returning_concrete_with_static_member_should_return_substitute_by_default()
+        {
+            var sub = Substitute.For<IInterfaceReturningConcreteWithPublicStaticMethod>();
+
+            var returnedClass = sub.AMethod();
+
+            Assert.That(returnedClass, Is.InstanceOf<ConcreteWithPublicStaticMethod>());
+        }
+
+        [Test]
+        public void Interface_returning_concrete_with_static_member_should_return_object_if_instructed_to()
+        {
+            var sub = Substitute.For<IInterfaceReturningConcreteWithPublicStaticMethod>();
+            var obj = new ConcreteWithPublicStaticMethod();
+            sub.AMethod().Returns(obj);
+
+            var returnedClass = sub.AMethod();
+
+            Assert.That(returnedClass, Is.SameAs(obj));
+        }
+    }
+
+    public class ConcreteWithPublicStaticMethod
+    {
+        public virtual string AProperty { get; set; }
+        public static void AStaticMethod() {}
+    }
+
+    public class ConcreteWithoutPublicStaticMethod
+    {
+        public virtual string AProperty { get; set; }
+    }
+
+    public interface IInterfaceReturningConcreteWithPublicStaticMethod
+    {
+        ConcreteWithPublicStaticMethod AMethod();
+    }
+
+    public interface IInterfaceReturningConcreteWithoutPublicStaticMethod
+    {
+        ConcreteWithoutPublicStaticMethod AMethod();
+    }
+}
