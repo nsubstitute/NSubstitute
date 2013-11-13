d4551b846079a8bce140cd6a96ccb4b453317c05
David Tchepak <dave@davesquared.net>
2013-11-13 21:47:25 +1100

    Adding some more tests for #114

--- Source/NSubstitute.Acceptance.Specs/FieldReports/Issue114_ArgumentCheckOfOptionalParameter.cs
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue114_ArgumentCheckOfOptionalParameter.cs
@@ -14,7 +14,30 @@ namespace NSubstitute.Acceptance.Specs.FieldReports
         {
             var substitute = Substitute.For<IInterface>();
             substitute.MethodWithOptionalParameter(new object());
-            substitute.ReceivedWithAnyArgs().MethodWithOptionalParameter(Arg.Any<object>());
+            substitute.Received().MethodWithOptionalParameter(Arg.Any<object>());
+            substitute.Received().MethodWithOptionalParameter(Arg.Any<object>(), null);
+            substitute.Received().MethodWithOptionalParameter(Arg.Any<object>());
+            substitute.ReceivedWithAnyArgs().MethodWithOptionalParameter(null);
+            substitute.ReceivedWithAnyArgs().MethodWithOptionalParameter(null, null);
+        }
+
+        [Test]
+        public void MatchWhenAllArgumentsSpecified()
+        {
+            var substitute = Substitute.For<IInterface>();
+            substitute.MethodWithOptionalParameter(new object(), 2);
+            substitute.Received().MethodWithOptionalParameter(Arg.Any<object>(), Arg.Any<object>());
+            substitute.Received().MethodWithOptionalParameter(Arg.Any<object>(), 2);
+        }
+
+        [Test]
+        public void DetectsMismatchedArgs()
+        {
+            var substitute = Substitute.For<IInterface>();
+            substitute.MethodWithOptionalParameter(new object(), 2);
+            substitute.DidNotReceive().MethodWithOptionalParameter(Arg.Any<object>());
+            substitute.DidNotReceive().MethodWithOptionalParameter(Arg.Any<object>(), 3);
+            substitute.DidNotReceive().MethodWithOptionalParameter(Arg.Any<object>(), "asdf");
         }
     }
 }
