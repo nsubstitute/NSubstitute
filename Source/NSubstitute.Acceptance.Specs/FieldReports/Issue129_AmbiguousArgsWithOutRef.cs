5becb88c21c9d23f143fa9109e2d26ce26fe0c6a
David Tchepak <dave@davesquared.net>
2014-01-06 22:30:04 +1100

    Repro for ambiguous args with out/ref bug (#129)

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue129_AmbiguousArgsWithOutRef.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue129_AmbiguousArgsWithOutRef
+    {
+        public interface ITestInterface
+        {
+            bool DoStuff(string input, out string output);
+        }
+
+        [Test]
+        [Pending, Explicit]
+        public void Test()
+        {
+            string someType;
+            var substitute = Substitute.For<ITestInterface>();
+            substitute.DoStuff(Arg.Any<string>(), out someType).ReturnsForAnyArgs(info =>
+            {
+                info[1] = "test";
+                return true;
+            });
+
+            string outString;
+            var result =substitute.DoStuff("a", out outString);
+
+            Assert.That(result);
+            Assert.That(outString, Is.EqualTo("test"));
+        }
+
+        [Test]
+        public void WorkAround()
+        {
+            string someType;
+            var substitute = Substitute.For<ITestInterface>();
+            var arg0 = Arg.Any<string>();
+            var arg1 = Arg.Any<string>();
+            substitute.DoStuff(arg0, out someType).ReturnsForAnyArgs(info =>
+            {
+                info[1] = "test";
+                return true;
+            });
+
+            string outString;
+            var result =substitute.DoStuff("a", out outString);
+
+            Assert.That(result);
+            Assert.That(outString, Is.EqualTo("test"));
+        }
+    }
+}
