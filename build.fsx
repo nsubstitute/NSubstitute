edd3819ec8059e94cf6c8486c81c21be3e0b0d3f
Brendan Forster <brendan@github.com>
2014-01-20 23:55:15 -0800

    clean and build

--- build.fsx
+++ build.fsx
@@ -2,4 +2,30 @@
 open Fake 
 open System
 
-let buildMode = getBuildParamOrDefault "buildMode" "Release"
+let ALL_TARGETS = ["NET35"; "NET40"]
+let EXPERIMENTAL_TARGETS = []
+
+let platform = getBuildParamOrDefault "platform" ALL_TARGETS.Head
+let buildMode = getBuildParamOrDefault "buildMode" "Debug"
+let config = String.Format("{0}-{1}", platform, buildMode)
+
+let SOURCE_PATH = "./Source"
+let OUTPUT_PATH = "./Output"
+
+Target "Clean" (fun _ ->
+     MSBuild null "Clean" ["Configuration", config] ["./Source/NSubstitute.2010.sln"]
+      |> Log "Clean: "
+)
+
+Target "BuildSolution" (fun _ ->
+    MSBuild null "Build" ["Configuration", config] ["./Source/NSubstitute.2010.sln"]
+    |> Log "Build: "
+)
+
+Target "Default" DoNothing
+
+"Clean"
+   ==> "BuildSolution"
+   ==> "Default"
+
+RunTargetOrDefault "Default"
