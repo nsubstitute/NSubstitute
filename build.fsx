0d603b2112e9d11c3158504286cdb4d753a09e5b
Brendan Forster <brendan@github.com>
2014-01-21 10:51:37 -0800

    run all the tests

--- build.fsx
+++ build.fsx
@@ -13,8 +13,7 @@ let SOURCE_PATH = "./Source"
 let OUTPUT_PATH = "./Output"
 
 Target "Clean" (fun _ ->
-     MSBuild null "Clean" ["Configuration", config] ["./Source/NSubstitute.2010.sln"]
-      |> Log "Clean: "
+    CleanDirs [ OUTPUT_PATH ]
 )
 
 Target "BuildSolution" (fun _ ->
@@ -22,10 +21,24 @@ Target "BuildSolution" (fun _ ->
     |> Log "Build: "
 )
 
+let testDir = String.Format("{0}/{1}/{2}/", OUTPUT_PATH, buildMode, platform)
+let testDlls = !! (testDir + "/**/*Specs.dll")
+
+Target "Test" (fun _ ->
+testDlls
+        |>  NUnit (fun p ->
+            {p with
+                DisableShadowCopy = true;
+                Framework = "net-4.0";
+                ExcludeCategory = "Pending";
+                OutputFile = testDir + "TestResults.xml"}) // TODO: different file name based on path
+)
+
 Target "Default" DoNothing
 
 "Clean"
    ==> "BuildSolution"
+   ==> "Test"
    ==> "Default"
 
 RunTargetOrDefault "Default"
