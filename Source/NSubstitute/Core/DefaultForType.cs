b349881c0587ebd7c992c6c82c9a2c7dde6b1bdf
Paul Betts <paul@paulbetts.org>
2014-01-20 21:04:28 -0800

    Make that actually true

--- Source/NSubstitute/Core/DefaultForType.cs
+++ Source/NSubstitute/Core/DefaultForType.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace NSubstitute.Core
 {
@@ -8,6 +10,32 @@ namespace NSubstitute.Core
         {
             if (IsVoid(type)) return null;
             if (type.IsValueType) return DefaultInstanceOfValueType(type);
+
+            if (type == typeof(Task)) 
+            {
+                // NB: http://stackoverflow.com/questions/11969208/non-generic-taskcompletionsource-or-alternative
+                TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+                tcs.SetResult(null);
+                return tcs.Task;
+            }
+
+            if (typeof(Task).IsAssignableFrom(type) && type.IsGenericType) 
+            {
+                Type innerType = type.GetGenericArguments()[0];
+                Type tcsType = typeof(TaskCompletionSource<>).MakeGenericType(innerType);
+                object tcs = Activator.CreateInstance(tcsType);
+
+                object result = innerType.IsValueType ? 
+                    DefaultInstanceOfValueType(innerType) :
+                    null;
+
+                MethodInfo setResult = tcsType.GetMethod("SetResult");
+                setResult.Invoke(tcs, new[] { result });
+
+                PropertyInfo task = tcsType.GetProperty("Task");
+                return task.GetGetMethod().Invoke(tcs, null);
+            }
+
             return null;
         }
 
