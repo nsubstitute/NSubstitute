036540f2d46f99488acfc5ddce6604b5161be55a
David Tchepak <dave@davesquared.net>
2014-04-25 23:55:20 +1000

    Updated build.bat to pass through cmd line args

--- build.bat
+++ build.bat
@@ -2,13 +2,7 @@
 
 "ThirdParty\NuGet\nuget.exe" "install" "FAKE.Core" "-OutputDirectory" "ThirdParty\FAKE" "-ExcludeVersion" "-version" "2.6.0"
 
-SET TARGET="Default"
-IF NOT [%1]==[] (set TARGET="%1")
-
-SET BUILDMODE="Release"
-IF NOT [%2]==[] (set BUILDMODE="%2")
-
-"ThirdParty\FAKE\FAKE.Core\tools\Fake.exe" "build.fsx" "target=%TARGET%" "buildMode=%BUILDMODE%"
+"ThirdParty\FAKE\FAKE.Core\tools\Fake.exe" "build.fsx" %*
 
 rem Bail if we're running a TeamCity build.
 if defined TEAMCITY_PROJECT_NAME goto Quit
