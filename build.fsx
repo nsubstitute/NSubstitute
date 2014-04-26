bb7f122cf698dfcb9a5b99f899cd57d5c9509420
David Tchepak <dave@davesquared.net>
2014-04-26 22:59:58 +1000

    Bit of cleanup of paths in tasks
    - Switched to using (@@) for path concat
    - Added "-T" task to list targets (similar to rake -T)
    - Removed hardcode of nsub binary paths for zip and nuget tasks

--- build.fsx
+++ build.fsx
@@ -22,17 +22,16 @@ module ExamplesToCode =
     
     let ConvertFile file targetDir = 
         let fileName = Path.GetFileNameWithoutExtension(file)
-        let target = (String.Format("{0}{1}.cs", targetDir, fileName))
-        log (String.Format("Converting {0} to {1}", file, target))
+        let target = targetDir @@ fileName + ".cs"
+        log <| sprintf "Converting %s to %s" file target
         TransformFile file target (ExtractDocs.strToFixture fileName)
     
     let Convert paths targetDir = 
         let paths = paths |> Seq.toList
         for p in paths do
-            trace (String.Format("Convert from {0} to {1}", p, targetDir))
-            let mdFiles = !!(p + "*.markdown") |> Seq.toList
-            let htmlFiles = !!(p + "*.html") |> Seq.toList
-            for file in List.append mdFiles htmlFiles do
+            trace <| sprintf "Convert from %s to %s" p targetDir
+            let files = !! "*.markdown" ++ "*.html" |> SetBaseDir p
+            for file in files do
                 ConvertFile file targetDir
 
 let ALL_TARGETS = [ "NET35"; "NET40"; "NET45" ]
@@ -45,12 +44,14 @@ let targets =
     | s -> s.Split([| ',' |], StringSplitOptions.RemoveEmptyEntries) |> Array.toList
 
 let OUTPUT_PATH = "Output"
-let outputDir = OUTPUT_PATH @@ buildMode
 let releaseNotes = ReadFile "CHANGELOG.txt" |> ReleaseNotesHelper.parseReleaseNotes
 let version = releaseNotes.AssemblyVersion
+let outputBasePath = OUTPUT_PATH @@ buildMode
+let deployPath = outputBasePath @@ "NSubstitute-" + version
 
-Target "Clean" (fun _ -> CleanDirs [ OUTPUT_PATH ])
-Target "Version" (fun _ -> 
+Target "Clean" <| fun _ -> CleanDirs [ OUTPUT_PATH ]
+
+Target "Version" <| fun _ ->
     CreateCSharpAssemblyInfo "Source/NSubstitute/Properties/AssemblyInfo.cs"
         [ Attribute.Title "NSubstitute"
           Attribute.Description "A simple substitute for .NET mocking libraries." 
@@ -58,40 +59,31 @@ Target "Version" (fun _ ->
           Attribute.Product "NSubstitute" 
           Attribute.Copyright @"Copyright \u00A9 2009 NSubstitute Team"
           Attribute.Version version
-          Attribute.FileVersion version ])
+          Attribute.FileVersion version ]
 
-Target "BuildSolution" (fun _ -> 
+Target "BuildSolution" <| fun _ -> 
     let build config mode = 
         MSBuild null "Build"
             [ "Configuration", config + "-" + buildMode ] // e.g. NET35-Debug
             [ "./Source/NSubstitute.2010.sln" ]
             |> ignore
-    targets |> List.iter (fun config -> build config buildMode))
+    targets |> List.iter (fun config -> build config buildMode)
 
-Target "Test" (fun _ -> 
+Target "Test" <| fun _ -> 
     targets
     |> List.iter (fun config -> 
-                   let workingDir = outputDir @@ config
-                   let testDlls = !!(workingDir @@ "**/*Specs.dll")
+                   let workingDir = outputBasePath @@ config
+                   let testDlls = !!(workingDir @@ "**" @@ "*Specs.dll")
                    testDlls |> NUnit(fun p -> 
                                    { p with DisableShadowCopy = true
                                             Framework = "net-4.0"
                                             ExcludeCategory = "Pending"
-                                            OutputFile = workingDir @@ "TestResults.xml" }) // TODO: different file name based on test assembly
-       )
-)
+                                            OutputFile = workingDir @@ "TestResults.xml" })
+                   )
 
 Target "Default" DoNothing
 
-let outputBasePath = OUTPUT_PATH @@ buildMode
-let workingDir = outputBasePath @@ "package"
-let net35binary = String.Format("{0}NET35/NSubstitute/NSubstitute.dll", outputDir)
-let net40binary = String.Format("{0}NET40/NSubstitute/NSubstitute.dll", outputDir)
-let net35binariesDir = String.Format("{0}lib/net35", workingDir)
-let net40binariesDir = String.Format("{0}lib/net40", workingDir)
-
-Target "Package" (fun _ -> 
-    let deployPath = outputBasePath @@ "NSubstitute-" + version
+Target "Package" <| fun _ -> 
     targets
     |> List.map (fun x -> x, deployPath @@ "lib" @@ x.ToLower())
     |> List.iter (fun (target, path) -> 
@@ -108,37 +100,25 @@ Target "Package" (fun _ ->
     tidyUpTextFileFromMarkdown("#{deploy_path}/README.txt")
     tidyUpTextFileFromMarkdown("#{deploy_path}/acknowledgements.txt")
     *)
-)
 
-Target "NuGet" (fun _ -> 
-    let workingDir = outputBasePath @@ "package"
-    targets
-    |> List.map (fun x -> workingDir @@ "lib" @@ x.ToLower())
-    |> List.iter CreateDir
-    let z = CopyFiles
-    CopyFile net35binariesDir net35binary
-    CopyFile net40binariesDir net40binary
+Target "NuGet" <| fun _ -> 
+    let nugetPath = outputBasePath @@ "nuget"
+    let workingDir = nugetPath @@ version
+    CreateDir workingDir
+    cp_r deployPath workingDir
+
     NuGet (fun p -> 
-        { p with OutputPath = outputBasePath
+        { p with OutputPath = nugetPath
                  WorkingDir = workingDir
                  Version = version
-                 ReleaseNotes = toLines releaseNotes.Notes }) "Build/NSubstitute.nuspec")
+                 ReleaseNotes = toLines releaseNotes.Notes }) "Build/NSubstitute.nuspec"
 
-// TODO: this is crazy hideous
-Target "Zip" (fun _ -> 
-    // porting markdown files as-is
-    CopyFile (workingDir + "acknowledgements.txt") "acknowledgements.markdown"
-    CopyFile workingDir "BreakingChanges.txt"
-    CopyFile workingDir "ChangeLog.txt"
-    CopyFile workingDir "LICENSE.txt"
-    CopyFile (workingDir + "README.txt") "README.markdown"
-    CreateDir net35binariesDir
-    CreateDir net40binariesDir
-    CopyFile net35binariesDir net35binary
-    CopyFile net40binariesDir net40binary
-    let outputZip = (outputBasePath + "NSubstitute." + version + ".zip")
-    !!(workingDir + "/**/*.*") -- "*.zip"
-    |> Zip workingDir outputZip)
+Target "Zip" <| fun _ -> 
+    let zipPath = outputBasePath @@ "zip"
+    CreateDir zipPath
+    let outputZip = (zipPath @@ "NSubstitute." + version + ".zip")
+    !!(deployPath @@ "**" @@ "*.*") -- "*.zip"
+    |> Zip deployPath outputZip
 
 Target "Documentation" (fun _ -> 
     log "building site..."
@@ -149,23 +129,24 @@ Target "Documentation" (fun _ ->
             info.Arguments <- "\"" + outputBasePath @@ "nsubstitute.github.com" + "\"")
     log ("returned" + result.ToString()))
 
-Target "CodeFromDocumentation" (fun _ -> 
-    let outputCodePath = String.Format("{0}/{1}/", OUTPUT_PATH, "CodeFromDocs")
+Target "CodeFromDocumentation" <| fun _ -> 
+    let outputCodePath = outputBasePath @@ "CodeFromDocs"
     CreateDir outputCodePath
     // generate samples from docs
     ExamplesToCode.Convert [ "./Source/Docs/"; "./Source/Docs/help/_posts/"; "./" ] outputCodePath
     // compile code samples
-    let basePath = (OUTPUT_PATH + "/" + buildMode + "/NET35")
+    let basePath = outputBasePath @@ "/NET35"
     
-    let references = 
-        [ for x in [ "NSubstitute.dll"; "nunit.framework.dll" ] -> 
-              String.Format("{0}/NSubstitute.Specs/{1}", basePath, x) ]
+    let references = [ "NSubstitute.dll"; "nunit.framework.dll" ] |> List.map (fun x -> basePath @@ "NSubstitute.Specs" @@ x)
     CopyFiles outputCodePath references
     CopyFile outputCodePath "./Build/samples.csproj"
-    MSBuild null "Build" [ "TargetFrameworkVersion", "v3.5" ] [ outputCodePath + "/samples.csproj" ] |> Log "Build: ")
+    MSBuild null "Build" [ "TargetFrameworkVersion", "v3.5" ] [ outputCodePath + "/samples.csproj" ] |> Log "Build: "
 
 // empty target to encompass doing everything
 Target "Release" DoNothing
+
+// list targets, similar to `rake -T`
+Target "-T" PrintTargets
 
 "Clean"
     ==> "Version"
@@ -175,7 +156,8 @@ Target "Release" DoNothing
     ==> "Package"
     ==> "NuGet"
     ==> "Zip" 
-    ==> "Documentation"
     ==> "CodeFromDocumentation"
+    ==> "Documentation"
     ==> "Release"
+
 RunTargetOrDefault "Default"
