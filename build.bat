fd69081eb66e6d4d2f28a350622bcfb87bf5a937
David Tchepak <dave@davesquared.net>
2014-07-10 20:51:57 +1000

    Restoring rake build.bat, moving fake build to build-fake.bat
    Keeping both rake and fake builds until we've sure fake build is ok.

--- build.bat
+++ build.bat
@@ -1,14 +1,4 @@
 @echo off
-
-"ThirdParty\NuGet\nuget.exe" "install" "FAKE.Core" "-OutputDirectory" "ThirdParty\FAKE" "-ExcludeVersion" "-version" "2.6.0"
-
-"ThirdParty\FAKE\FAKE.Core\tools\Fake.exe" "build.fsx" %*
-
-rem Bail if we're running a TeamCity build.
-if defined TEAMCITY_PROJECT_NAME goto Quit
-
-rem Bail if we're running a MyGet build.
-if /i "%BuildRunner%"=="MyGet" goto Quit
-
-:Quit
-exit /b %errorlevel%
+cd Build
+bundle exec rake %*
+cd ..
