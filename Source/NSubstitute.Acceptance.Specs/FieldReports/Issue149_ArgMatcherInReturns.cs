15263e7d46b9b426a9f07e67f945ead709e17a16
David Tchepak <dave@davesquared.net>
2014-07-13 23:16:14 +1000

    Adding test for #149

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue149_ArgMatcherInReturns.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue149_ArgMatcherInReturns
+    {
+        public interface ISub1 { Item GetItem(string s); }
+        public interface ISub2 { string GetSignature(int i); }
+        public class Item { }
+
+        [Test] [Pending] [Ignore]
+        public void MatcherInReturns()
+        {
+            var sub1 = Substitute.For<ISub1>();
+            var sub2 = Substitute.For<ISub2>();
+            sub1.GetItem(Arg.Any<string>()).Returns(new Item());
+            sub2.GetSignature(1).Returns(Arg.Any<string>()); // <-- THIS IS THE PROBLEM
+
+            sub1.GetItem("mystring");
+
+            sub1.ReceivedWithAnyArgs(1).GetItem("mystring");
+        }
+    }
+}
