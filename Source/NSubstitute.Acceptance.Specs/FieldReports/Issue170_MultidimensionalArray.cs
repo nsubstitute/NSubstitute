551155fe004755731086d5af9fd6ae782236a421
Alexandr Nikitin <alexandr.nikitin@adform.com>
2014-12-25 17:10:33 +0200

    Added a test for #170 (Can't create a substitute for a method returning multidimensional array)

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue170_MultidimensionalArray.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue170_MultidimensionalArray
+    {
+        public interface ITest
+        {
+            bool[,] Method();
+        }
+
+        [Test]
+        [Pending, Explicit]
+        public void Method_Works()
+        {
+            var test = Substitute.For<ITest>();
+            test.Method();
+        }
+    }
+}
