5d1e20e8a7316ec0713f913d18778077c0588b31
Bartosz Gołek <bartosz.golek@gmail.com>
2015-09-14 12:52:23 +0200

    Added ability to consecutive calls for WhenCalled.Do method

---
+++ Source/NSubstitute/Extensions/ConsecutiveDoExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute.Core;
+
+namespace NSubstitute.Extensions
+{
+	public static class ConsecutiveDoExtension
+	{
+		public static void Do<T>(this WhenCalled<T> whenCalled, params Action<CallInfo>[] calls)
+		{
+			var cc = new ConsecutiveCall(calls);
+			whenCalled.Do(cc.Call);
+		}
+
+		private class ConsecutiveCall
+		{
+			private readonly Stack<Action<CallInfo>> actions;
+
+			public ConsecutiveCall(params Action<CallInfo>[] actions)
+			{
+				this.actions = new Stack<Action<CallInfo>>(actions);
+			}
+
+			public void Call(CallInfo ci)
+			{
+				actions.Pop().Invoke(ci);
+			}
+		}
+	}
+}
