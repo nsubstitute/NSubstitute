0a1e70683aa5ce96f1434006b50275fba314b4ea
David Mann <dave@dmsquared.com>
2015-09-30 16:29:57 -0500

    Add [Serializable] attribute
    Enables other frameworks to report exceptions across application domains using the default binary serializer.

--- Source/NSubstitute/Exceptions/SubstituteException.cs
+++ Source/NSubstitute/Exceptions/SubstituteException.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 
 namespace NSubstitute.Exceptions
 {
+    [Serializable]
     public class SubstituteException : Exception
     {
         public SubstituteException() : this("") { }
