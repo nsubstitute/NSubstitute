baf9cc665708da20332289d3e9f14148679c2064
David Mann <dave@dmsquared.com>
2015-09-30 16:23:44 -0500

    Add [Serializable] attribute
    Enables other frameworks to report exceptions across application domains using the default binary serializer.

--- Source/NSubstitute/Exceptions/ArgumentNotFoundException.cs
+++ Source/NSubstitute/Exceptions/ArgumentNotFoundException.cs
@@ -2,6 +2,7 @@ using System.Runtime.Serialization;
 
 namespace NSubstitute.Exceptions
 {
+    [Serializable]
     public class ArgumentNotFoundException : SubstituteException
     {
         public ArgumentNotFoundException(string message) : base(message) { }
