30ccd23201f6a501362f9d3b12e2a8aee4e5a6f3
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-10-14 17:06:28 +0300

    Fix CallRouterResolver threadsafety issue

--- Source/NSubstitute/Core/CallRouterResolver.cs
+++ Source/NSubstitute/Core/CallRouterResolver.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+#if NET4
+using System.Collections.Concurrent;
+#endif
 using System.Collections.Generic;
 using NSubstitute.Exceptions;
 
@@ -6,7 +9,11 @@ namespace NSubstitute.Core
 {
     public class CallRouterResolver : ICallRouterResolver
     {
+#if NET4
+        IDictionary<object, ICallRouter> _callRouterMappings = new ConcurrentDictionary<object, ICallRouter>();
+#else
         IDictionary<object, ICallRouter> _callRouterMappings = new Dictionary<object, ICallRouter>();
+#endif
 
         public ICallRouter ResolveFor(object substitute)
         {
@@ -25,7 +32,15 @@ namespace NSubstitute.Core
         {
             if (proxy is ICallRouter) return;
             if (proxy is ICallRouterProvider) return;
+
+#if NET4
             _callRouterMappings.Add(proxy, callRouter);
+#else
+            lock (_callRouterMappings)
+            {
+                _callRouterMappings.Add(proxy, callRouter);
+            }
+#endif
         }
     }
 }
