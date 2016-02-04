abc896aaa85aecacd051347778a8a3a45894062a
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-02-04 11:05:15 +0200

    Change CoreCLR version to "stable" one

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ rem todo do not install dnvm all the time
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 
 rem todo remove hardcoded version
-CALL dnvm install '1.0.0-rc2-16319' -r coreclr -arch x64 -a default
+CALL dnvm install '1.0.0-rc1-update1' -r coreclr -arch x64 -alias nsubstitute-netcore
 
-CALL dnvm use default -runtime coreclr -arch x64
-SET DNX_FOLDER=%USERPROFILE%\.dnx\runtimes\dnx-coreclr-win-x64.1.0.0-rc2-16319\bin
+CALL dnvm use nsubstitute-netcore -runtime coreclr
+SET DNX_FOLDER=%USERPROFILE%\.dnx\runtimes\dnx-coreclr-win-x64.1.0.0-rc1-update1\bin
 
 "ThirdParty\FAKE\FAKE.Core\tools\Fake.exe" "build.fsx" %*
 
