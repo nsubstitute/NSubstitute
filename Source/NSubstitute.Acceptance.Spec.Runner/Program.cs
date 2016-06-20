e0f973ca5c35f0a0692e9be691a2871aafc7e52c
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-06-20 22:54:43 +0300

    Add a runner for the unit tests

---
+++ Source/NSubstitute.Acceptance.Spec.Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using NUnit.Common;
+using NUnitLite;
+
+namespace NSubstitute.Acceptance.Spec.Runner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+               .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+        }
+    }
+}
