bff5f2fb4258d481ddeb22a9edf51bc20833f85d
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2016-07-01 11:34:21 +0300

    Test runner

--- Source/NSubstitute.Acceptance.Specs/Program.cs
+++ Source/NSubstitute.Acceptance.Specs/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Reflection;
-using NUnit.Common;
-using NUnitLite;
-
-namespace NSubstitute.Acceptance.Specs
+﻿namespace NSubstitute.Acceptance.Specs
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            new AutoRun(typeof(Program).GetTypeInfo().Assembly)
-               .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+            // TODO remove this file
         }
     }
 }
