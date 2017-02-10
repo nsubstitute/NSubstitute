aad91cada5fd557843ff66b38e892d0d6749bd2f
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-02-10 20:42:28 +0200

    Add a failing test for multiple return values requested in parallel. A concise repro for #282.

---
+++ Source/NSubstitute.Acceptance.Specs/FieldReports/Issue282_MultipleReturnValuesParallelism.cs
@@ -0,0 +1,35 @@
+﻿#if NET45
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    [TestFixture]
+    public class Issue282_MultipleReturnValuesParallelism
+    {
+        public interface IFoo
+        {
+            string Foo();
+        }
+
+        [Test]
+        [Ignore("#282")]
+        public async void ReturnsMultipleValuesInParallel()
+        {
+            var ret1 = "One";
+            var ret2 = "Two";
+
+            var substitute = Substitute.For<IFoo>();
+            substitute.Foo().Returns(ret1, ret2);
+
+            var runningTask1 = Task.Run(() => substitute.Foo());
+            var runningTask2 = Task.Run(() => substitute.Foo());
+
+            var results = await Task.WhenAll(runningTask1, runningTask2);
+
+            Assert.Contains(ret1, results);
+            Assert.Contains(ret2, results);
+        }
+    }
+}
+#endif
