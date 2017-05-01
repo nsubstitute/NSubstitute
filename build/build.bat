aa4a84be27ba9e7f9b574c509dd940d50a3e76b8
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-05-01 19:13:16 +0300

    Build scripts

--- build/build.bat
+++ build/build.bat
@@ -2,15 +2,7 @@
 
 cls
 set encoding=utf-8
-nuget.exe restore packages.config -PackagesDirectory ..\packages
-
-"..\packages\FAKE.4.60.0\tools\Fake.exe" build.fsx 
-
-rem Bail if we're running a TeamCity build.
-if defined TEAMCITY_PROJECT_NAME goto Quit
-
-rem Bail if we're running a MyGet build.
-if /i "%BuildRunner%"=="MyGet" goto Quit
-
-:Quit
-exit /b %errorlevel%
+pushd %~dp0..\
+"build/nuget.exe" restore "build/packages.config " -PackagesDirectory "packages"
+"packages/FAKE.4.60.0/tools/Fake.exe" "build/build.fsx"
+popd
--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,11 @@
-"ThirdParty/FAKE/FAKE.Core/tools/Fake.exe" "build.fsx" $*
+#!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+mono $SCRIPT_PATH/../packages/FAKE.4.58.5/tools/FAKE.exe BuildTests --fsiargs -d:MONO $SCRIPT_PATH/build.fsx
