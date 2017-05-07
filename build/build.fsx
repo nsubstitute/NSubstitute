cf52db1aeb036229c3c63191d72c714b03fe9969
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-05-07 12:46:40 +0300

    Refactor Travis CI

--- build/build.fsx
+++ build/build.fsx
@@ -1,19 +1,25 @@
 #r @"../packages/FAKE.4.60.0/tools/FakeLib.dll"
 
 open Fake
+open System
 
+let solutionFile  = "NSubstitute.sln"
 let configuration = getBuildParamOrDefault "configuration" "Debug"
 
 let rootDir = "../"
 let outputBaseDir = rootDir @@ "output/"
 let outputDir = outputBaseDir @@ configuration
 
-Target "Clean" <| fun _ -> CleanDirs [ outputDir ]
-
 Target "Default" DoNothing
 Target "All" DoNothing
 
-// .NET Core build
+let vsProjProps = [ ("Configuration", configuration); ("Platform", "Any CPU") ]
+
+Target "Clean" (fun _ ->
+    !! solutionFile |> MSBuildReleaseExt "" vsProjProps "Clean" |> ignore
+    CleanDirs [ outputBaseDir ]
+)
+
 Target "Restore" (fun _ ->
     DotNetCli.Restore (fun p -> 
         { p with 
@@ -21,9 +27,9 @@ Target "Restore" (fun _ ->
 )
 
 Target "Build" (fun _ ->
-    DotNetCli.Build (fun p -> 
-        { p with 
-            Configuration = configuration })
+    !! solutionFile
+    |> MSBuildReleaseExt "" vsProjProps "Rebuild"
+    |> ignore
 )
 
 Target "Test" (fun _ ->
