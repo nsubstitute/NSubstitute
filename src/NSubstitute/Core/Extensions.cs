48fa806de0c8bca18b3aa6b1a4af99eaf7108457
David Tchepak <dave@davesquared.net>
2017-11-20 11:15:08 +1100

    Removed Extensions.Zip
    No longer required since dropping support for NET35.
    Also fixed some R# suggestions in the file.
    
    Closes #336.

--- src/NSubstitute/Core/Extensions.cs
+++ src/NSubstitute/Core/Extensions.cs
@@ -8,34 +8,6 @@ namespace NSubstitute.Core
     public static class Extensions
     {
         /// <summary>
-        /// Combines two enumerables into a new enumerable using the given selector.
-        /// </summary>
-        /// <typeparam name="TFirst"></typeparam>
-        /// <typeparam name="TSecond"></typeparam>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
-        /// <param name="selector"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// This implementation was sanity-checked against the 
-        /// <a href="http://msmvps.com/blogs/jon_skeet/archive/2011/01/14/reimplementing-linq-to-objects-part-35-zip.aspx">Edulinq implementation</a> and
-        /// <a href="http://blogs.msdn.com/b/ericlippert/archive/2009/05/07/zip-me-up.aspx">Eric Lippert's implementation</a>.
-        /// </remarks>
-        public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>(
-            this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> selector)
-        {
-            using (var firstEnumerator = first.GetEnumerator())
-            using (var secondEnumerator = second.GetEnumerator())
-            {
-                while (firstEnumerator.MoveNext() && secondEnumerator.MoveNext())
-                {
-                    yield return selector(firstEnumerator.Current, secondEnumerator.Current);
-                }
-            }
-        }
-
-        /// <summary>
         /// Checks if the instance can be used when a <paramref name="type"/> is expected.
         /// </summary>
         /// <param name="instance"></param>
@@ -43,7 +15,7 @@ namespace NSubstitute.Core
         /// <returns></returns>
         public static bool IsCompatibleWith(this object instance, Type type)
         {
-            var requiredType = (type.IsByRef) ? type.GetElementType() : type;
+            var requiredType = type.IsByRef ? type.GetElementType() : type;
             return instance == null ? TypeCanBeNull(requiredType) : requiredType.IsInstanceOfType(instance);
         }
 
@@ -65,8 +37,10 @@ namespace NSubstitute.Core
 
         public static Maybe<T> FirstOrNothing<T>(this IEnumerable<T> items)
         {
-            var enumerator = items.GetEnumerator();
-            return enumerator.MoveNext() ? Maybe.Just(enumerator.Current) : Maybe.Nothing<T>();
+            using (var enumerator = items.GetEnumerator())
+            {
+                return enumerator.MoveNext() ? Maybe.Just(enumerator.Current) : Maybe.Nothing<T>();
+            }
         }
     }
 }
