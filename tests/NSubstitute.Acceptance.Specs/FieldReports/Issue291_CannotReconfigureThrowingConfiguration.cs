3360998138149f9e2a9a611f38324819cedde420
Alex Povar <zvirja1@gmail.com>
2018-02-02 19:07:50 +0200

    Fix one configuration could prevent from other configuration
    Previously we returned the already configured value for the
    specifying call. There is no sense to do that, as the returned
    value is later discarded when extensions like `Returns()` are used.
    Therefore, fixed issue by simply stopping to return the configured
    value is the specifying call.

---
+++ tests/NSubstitute.Acceptance.Specs/FieldReports/Issue291_CannotReconfigureThrowingConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NSubstitute.Acceptance.Specs.Infrastructure;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    public class Issue291_CannotReconfigureThrowingConfiguration
+    {
+        // Based on: http://stackoverflow.com/q/42686269/906
+
+        public interface IRequest { }
+        public interface IResponse { }
+        public interface IDeliver { IResponse Send(IRequest msg); }
+
+        public class Message1 : IRequest { }
+        public class Message2 : IRequest { }
+        public class Response : IResponse { }
+
+        [Test]
+        public void ShouldBePossibleToReConfigureThrowingConfiguration()
+        {
+            // Arrange
+            var response = new Response();
+            var deliver = Substitute.For<IDeliver>();
+            
+            // Act
+            deliver.Send(Arg.Any<Message1>()).Throws<InvalidOperationException>();
+            deliver.Send(Arg.Any<Message2>()).Returns(response);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => deliver.Send(new Message1()));
+            Assert.AreSame(response, deliver.Send(new Message2()));
+        }
+
+        [Test]
+        public void ShouldBePossibleToConfigureConstantAfterThrowForAny()
+        {
+            // Arrange
+            var something = Substitute.For<ISomething>();
+            
+            // Act
+            something.Echo(Arg.Any<int>()).Throws<InvalidOperationException>();
+            something.Echo(Arg.Is(42)).Returns("42");
+            
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => something.Echo(100));
+            Assert.AreEqual("42", something.Echo(42));
+        }
+    }
+}
