c930ab1e21479ec36f43b45722c019f9e6a2edba
Alex Povar <zvirja1@gmail.com>
2018-02-09 12:50:29 +0200

    Add the Configure() hint extension method
    The added extension method switches the next call route to the
    "RecordCallSpecification". That allows to avoid undesired side
    effects during the method configuration as route is very tiny.

---
+++ src/NSubstitute/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using NSubstitute.Core;
+
+namespace NSubstitute.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// A hint for the NSubstitute that the subsequent method/property call is about to be configured.
+        /// <para>
+        /// NOTICE, you _don't need_ to invoke this method for the basic configuration scenarios.
+        /// Ensure you don't overuse this method and it's applied only if strictly required.
+        /// </para>
+        /// <remarks>
+        /// Due to the NSubstitute configuration syntax it's often impossible to recognize during the method call
+        /// dispatch whether this is a setup phase or a regular method call.
+        /// Usually, it doesn't matter, however sometimes method invocation could lead to undesired side effects
+        /// (e.g. the previously configured value is returned, base method is invoked). In that case you might want to
+        /// provide NSubstitute with a hint that you are configuring the method, so it treats call in a special mode.
+        /// </remarks>
+        /// </summary>
+        public static T Configure<T>(this T substitute) where T : class
+        {
+            var context = SubstitutionContext.Current;
+            var router = context.GetCallRouterFor(substitute);
+            var routeFactory = context.GetRouteFactory();
+
+            router.SetRoute(state => routeFactory.RecordCallSpecification(state));
+
+            return substitute;
+        }
+    }
+}
