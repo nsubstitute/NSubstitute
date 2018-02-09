3ee90780caa32614e2159d4d507de01a016ff1ce
Alex Povar <zvirja1@gmail.com>
2018-02-09 16:37:58 +0200

    Simplify code debugging by showing kind for MatchArgs

--- src/NSubstitute/Core/MatchArgs.cs
+++ src/NSubstitute/Core/MatchArgs.cs
@@ -1,10 +1,21 @@
+using System.Diagnostics;
+
 namespace NSubstitute.Core
 {
+    [DebuggerDisplay("{" + nameof(GetDebugDisplayName) + "}")]
     public class MatchArgs
     {
-        private MatchArgs() { }
-
         public static readonly MatchArgs AsSpecifiedInCall = new MatchArgs();
         public static readonly MatchArgs Any = new MatchArgs();
+
+        private MatchArgs() { }
+
+        private string GetDebugDisplayName()
+        {
+            if (this == AsSpecifiedInCall) return nameof(AsSpecifiedInCall);
+            if (this == Any) return nameof(Any);
+
+            return "UNKNOWN";
+        }
     }
 }
