8487b65f7826cc983b0499f04e91d41e2a8ccc0a
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-03-05 15:18:57 +0200

    Add property tests for RedundantArgumentMatcherException

--- tests/NSubstitute.Acceptance.Specs/ArgumentMatching.cs
+++ tests/NSubstitute.Acceptance.Specs/ArgumentMatching.cs
@@ -287,6 +287,32 @@ namespace NSubstitute.Acceptance.Specs
         }
 
         [Test]
+        public void Should_fail_with_redundant_exception_if_more_specifications_than_arguments_scenario_3()
+        {
+            // This spec will be ignored, however it's good to let user know that test might not work how he expects.
+            Arg.Is(42);
+
+            var ex = Assert.Throws<RedundantArgumentMatcherException>(() =>
+            {
+                _something.SomeProperty = 24;
+            });
+            Assert.That(ex.Message, Contains.Substring("42"));
+        }
+
+        [Test]
+        public void Should_fail_with_redundant_exception_if_more_specifications_than_arguments_scenario_4()
+        {
+            // This spec will be ignored, however it's good to let user know that test might not work how he expects.
+            Arg.Is(42);
+
+            // TODO: What is the expected behavior here??
+            Assert.Throws<RedundantArgumentMatcherException>(() =>
+            {
+                var _ = _something.SomeProperty;
+            });
+        }
+
+        [Test]
         public void Redundant_argument_matcher_exception_should_contain_list_of_all_matchers()
         {
             Arg.Is(42);
