f51e26aa4025eb294e8096834d74b9935cc1386f
Alex Povar <zvirja@users.noreply.github.com>
2018-03-26 03:04:41 +0300

    Verify assembly is always signed (#385)

---
+++ tests/NSubstitute.Acceptance.Specs/AssemblySigningTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs
+{
+    public class AssemblySigningTest
+    {
+        [Test]
+        public void NSubstitute_assembly_should_be_signed()
+        {
+            var assemblyName = typeof(Substitute).GetTypeInfo().Assembly.GetName();
+            var publicKeyToken = string.Join("", assemblyName.GetPublicKeyToken().Select(x => x.ToString("x")));
+            
+            Assert.That(publicKeyToken, Is.EqualTo("92dd2e9066daa5ca"));
+        }
+    }
+}
