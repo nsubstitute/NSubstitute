236f641c32b4753a7e54a0a594600e24bd9a3af2
Alex Povar <zvirja1@gmail.com>
2018-03-27 11:54:41 +0300

    Extract CallInfoFactory construction to SubstituteContext
    Now it's possible to globally override the CallInfoFactory.

--- src/NSubstitute/Core/CallRouterFactory.cs
+++ src/NSubstitute/Core/CallRouterFactory.cs
@@ -7,14 +7,17 @@ namespace NSubstitute.Core
         private readonly SequenceNumberGenerator _sequenceNumberGenerator;
         private readonly IRouteFactory _routeFactory;
         private readonly ICallSpecificationFactory _callSpecificationFactory;
+        private readonly ICallInfoFactory _callInfoFactory;
 
         public CallRouterFactory(SequenceNumberGenerator sequenceNumberGenerator,
             IRouteFactory routeFactory,
-            ICallSpecificationFactory callSpecificationFactory)
+            ICallSpecificationFactory callSpecificationFactory,
+            ICallInfoFactory callInfoFactory)
         {
             _sequenceNumberGenerator = sequenceNumberGenerator;
             _routeFactory = routeFactory;
             _callSpecificationFactory = callSpecificationFactory;
+            _callInfoFactory = callInfoFactory;
         }
 
         public ICallRouter Create(SubstituteConfig config, IThreadLocalContext threadContext, ISubstituteFactory substituteFactory)
@@ -22,7 +25,9 @@ namespace NSubstitute.Core
             var substituteState = new SubstituteState(config,
                 _sequenceNumberGenerator,
                 substituteFactory,
-                _callSpecificationFactory);
+                _callSpecificationFactory,
+                _callInfoFactory);
+
             return new CallRouter(substituteState, threadContext, _routeFactory);
         }
     }
