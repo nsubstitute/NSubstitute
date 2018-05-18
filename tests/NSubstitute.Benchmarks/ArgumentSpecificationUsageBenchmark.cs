0ca094cec7b6b2bf4cf1cf33766aab6e0ee388d2
Alex Povar <zvirja1@gmail.com>
2018-05-18 22:55:32 +0300

    Add benchmark for method configuration

---
+++ tests/NSubstitute.Benchmarks/ArgumentSpecificationUsageBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using NSubstitute.Benchmarks.TestTypes;
+
+namespace NSubstitute.Benchmarks
+{
+    [ClrJob, CoreJob]
+    public class ArgumentSpecificationUsageBenchmark
+    {
+        private IInterfaceWithMultipleMethods _substitute = Substitute.For<IInterfaceWithMultipleMethods>();
+
+        [Benchmark]
+        public void ConfigureArgumentWithAnyValue()
+        {
+            _substitute.MethodWithArg(Arg.Any<int>());
+        }
+
+        [Benchmark]
+        public void ConfigureRefArgumentWithAnyValue()
+        {
+            var refValue = Arg.Any<int>();
+            _substitute.MethodWithRefArg(ref refValue);
+        }
+
+        [Benchmark]
+        public void ConfigureOutArgumentWithAnyValue()
+        {
+            var outValue = Arg.Any<int>();
+            _substitute.MethodWithOutArg(out outValue);
+        }
+    }
+}
