6f32c79a66b2ea6bbf864d629a0be5d13849a6ad
David Tchepak <dave@davesquared.net>
2018-06-07 21:47:51 +1000

    Adding test for #378

---
+++ tests/NSubstitute.Acceptance.Specs/FieldReports/Issue378_InValueTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports
+{
+    /// <summary>
+    /// Issue from https://github.com/nsubstitute/NSubstitute/issues/378.
+    /// </summary>
+    public class Issue378_InValueTypes
+    {
+        public readonly struct Struct { }
+
+        public interface IStructByRefConsumer { void Consume(in Struct message); }
+
+        public interface IStructByValueConsumer { void Consume(Struct message); }
+
+        [Test]
+        public void IStructByRefConsumer_Test()
+        {
+            _ = Substitute.For<IStructByRefConsumer>();
+        }
+
+        [Test]
+        public void IStructByValueConsumer_Test()
+        {
+            _ = Substitute.For<IStructByValueConsumer>();
+        }
+    }
+}
