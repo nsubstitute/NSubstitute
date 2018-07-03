daeec390fc3fcee2010937be05f922aa668add71
Alex Povar <1074182+zvirja@users.noreply.github.com>
2018-07-03 15:59:36 +0300

    Use statically defined IsReadOnlyAttribute type (#421)
    Previously we generated the IsReadOnlyAttribute at run time.
    The problem is that currenlty Castle.Core doesn't expose a way to synchronize
    type creation. As result it could happen that type is defined twice and creation
    fails with exception.
    
    Given that we don't have a reliable way to ensure that type is defined once only,
    we defined our own internal IsReadOnlyAttribute type and made it visible to the
    dynamic assembly.
    
    The reason this type is internal rather than public is that it might exist in the
    target platform, so we don't want our attribute to interfere with system one.

---
+++ src/NSubstitute/AssemblyProperties.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using Castle.Core.Internal;
+
+// Make internal types visible to Castle's dynamic assembly as we might want
+// to dynamically define internal IsReadOnlyAttribute attribute.
+[assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)]
