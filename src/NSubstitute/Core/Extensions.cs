211ee6798a451ed25042759148a64df6407f70cb
Alex Povar <zvirja1@gmail.com>
2018-10-01 11:40:10 +0300

    Set internal visibility to internal extensions

--- src/NSubstitute/Core/Extensions.cs
+++ src/NSubstitute/Core/Extensions.cs
@@ -5,14 +5,11 @@ using System.Reflection;
 
 namespace NSubstitute.Core
 {
-    public static class Extensions
+    internal static class Extensions
     {
         /// <summary>
         /// Checks if the instance can be used when a <paramref name="type"/> is expected.
         /// </summary>
-        /// <param name="instance"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
         public static bool IsCompatibleWith(this object instance, Type type)
         {
             var requiredType = type.IsByRef ? type.GetElementType() : type;
@@ -22,12 +19,9 @@ namespace NSubstitute.Core
         /// <summary>
         /// Join the <paramref name="strings"/> using <paramref name="seperator"/>.
         /// </summary>
-        /// <param name="strings"></param>
-        /// <param name="seperator"></param>
-        /// <returns></returns>
         public static string Join(this IEnumerable<string> strings, string seperator)
         {
-            return string.Join(seperator, strings.ToArray());
+            return string.Join(seperator, strings);
         }
 
         private static bool TypeCanBeNull(Type type)
@@ -35,15 +29,7 @@ namespace NSubstitute.Core
             return !type.GetTypeInfo().IsValueType || Nullable.GetUnderlyingType(type) != null;
         }
 
-        public static Maybe<T> FirstOrNothing<T>(this IEnumerable<T> items)
-        {
-            using (var enumerator = items.GetEnumerator())
-            {
-                return enumerator.MoveNext() ? Maybe.Just(enumerator.Current) : Maybe.Nothing<T>();
-            }
-        }
-
-        internal static string GetNonMangledTypeName(this Type type)
+        public static string GetNonMangledTypeName(this Type type)
         {
             var typeName = type.Name;
             if (!type.GetTypeInfo().IsGenericType)
