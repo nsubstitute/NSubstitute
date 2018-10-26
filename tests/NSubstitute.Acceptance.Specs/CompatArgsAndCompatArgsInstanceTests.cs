d556324078a94a8710e76dedd4791509fa343e29
David Tchepak <dave@davesquared.net>
2018-10-26 20:55:36 +1100

    Add CompatArgInstance for aiding migration to v4.0
    People with pre-C#7 may have to update larges numbers of tests to use
    CompatArg methods instead of Arg. This can be made a bit easier by
    adding:
    
      readonly CompatArgInstance Arg = new CompatArgInstance();
    
    To each fixture or to a single base fixture. The tests will pick up this
    Arg member rather than having to update multiple instances per fixture
    from Arg to CompatArg.

---
+++ tests/NSubstitute.Acceptance.Specs/CompatArgsAndCompatArgsInstanceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using NSubstitute.Compatibility;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs {
+
+    public class CompatArgsAndCompatArgsInstanceTests {
+
+        [Test]
+        public void CompatArgsAndInstanceShouldBeInSync() {
+            var flags = BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance;
+            var compatMembers =
+                typeof(CompatArg).GetMethods(flags).Select(DescribeMethod).OrderBy(x => x);
+            var compatInstanceMembers =
+                typeof(CompatArgInstance).GetMethods(flags).Select(DescribeMethod).OrderBy(x => x);
+
+            Assert.AreEqual(
+                compatMembers, compatInstanceMembers,
+                "CompatArgs and CompatArgsInstance should have static/instance versions of the same members"
+                );
+        }
+
+        private static string DescribeMethod(MethodInfo m) {
+            return $"{m.Name}<{DescribeTypeList(m.GetGenericArguments())}>({DescribeParameters(m.GetParameters())})";
+        }
+
+        private static string DescribeTypeList(Type[] args) {
+            return string.Join(", ", args.Select(x => x.Name));
+        }
+
+        private static string DescribeParameters(ParameterInfo[] parameters) {
+            return string.Join(", ", parameters.Select(x => $"{x.ParameterType.Name} {x.Name}"));
+        }
+
+    }
+}
