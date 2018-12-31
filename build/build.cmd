05ebb63a26bf9b082c53dae21cf601c473a4e45a
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-12-30 16:21:21 -0800

    Update FAKE in scripts

--- build/build.cmd
+++ build/build.cmd
@@ -8,4 +8,4 @@ SET SCRIPT_DIR=%~dp0
 "%SCRIPT_DIR%\nuget.exe" restore "%SCRIPT_DIR%\packages.config" -PackagesDirectory "%SCRIPT_DIR%\packages"
 
 REM If changing FAKE version, also update build.sh
-"%SCRIPT_DIR%\packages/FAKE.4.63.0/tools/Fake.exe" "%SCRIPT_DIR%\build.fsx" %*
+"%SCRIPT_DIR%\packages/FAKE.5.8.4/tools/Fake.exe" "%SCRIPT_DIR%\build.fsx" %*
--- build/build.sh
+++ build/build.sh
@@ -15,11 +15,11 @@ then
     $SCRIPT_PATH/nuget.exe restore $SCRIPT_PATH/packages.config -PackagesDirectory $SCRIPT_PATH/packages
 
     # If updating FAKE version, also update build.bat and build.fsx
-    $SCRIPT_PATH/packages/FAKE.4.63.0/tools/FAKE.exe $@ --fsiargs $SCRIPT_PATH/build.fsx
+    $SCRIPT_PATH/packages/FAKE.5.8.4/tools/FAKE.exe $@ --fsiargs $SCRIPT_PATH/build.fsx
 else
     mozroots --import --sync
     mono --runtime=v4.0 $SCRIPT_PATH/nuget.exe restore $SCRIPT_PATH/packages.config -PackagesDirectory $SCRIPT_PATH/packages
 
     # If updating FAKE version, also update build.bat and build.fsx
-    mono $SCRIPT_PATH/packages/FAKE.4.63.0/tools/FAKE.exe $@ --fsiargs -d:MONO $SCRIPT_PATH/build.fsx
+    mono $SCRIPT_PATH/packages/FAKE.5.8.4/tools/FAKE.exe $@ --fsiargs -d:MONO $SCRIPT_PATH/build.fsx
 fi
