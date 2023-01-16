550633910de41174a1b6a2b385059053caccc2f0
Romfos <Romfos@tut.by>
2023-01-16 17:09:36 +0100

    Update build.fsx #2

--- build/build.fsx
+++ build/build.fsx
@@ -170,9 +170,9 @@ target "TestCodeFromDocs" <| fun _ ->
             <LangVersion>latest</LangVersion>
           </PropertyGroup>
           <ItemGroup>
-            <PackageReference Include="Microsoft.NET.Test.Sdk" Version="15.3.0" />
-            <PackageReference Include="NUnit" Version="3.8.1" />
-            <PackageReference Include="NUnit3TestAdapter" Version="3.8.0" />
+            <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.4.1" />
+            <PackageReference Include="NUnit" Version="3.13.3" />
+            <PackageReference Include="NUnit3TestAdapter" Version="4.3.1" />
           </ItemGroup>
           <ItemGroup>
             <ProjectReference Include="..\..\..\src\NSubstitute\NSubstitute.csproj" />
