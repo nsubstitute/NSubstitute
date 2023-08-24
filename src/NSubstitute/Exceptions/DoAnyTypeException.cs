5cf222bac469df76cf9ac22adef3950e79e1b53d
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2023-08-24 21:56:20 +0200

    Removed unused DoAnyTypeException

--- src/NSubstitute/Exceptions/DoAnyTypeException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NSubstitute.Exceptions;
-
-public class DoAnyTypeException : SubstituteException
-{
-    private const string FixedMessage = "Use DoForAny() instead of Do<AnyType>()";
-    public DoAnyTypeException() : base(FixedMessage) { }
-}
