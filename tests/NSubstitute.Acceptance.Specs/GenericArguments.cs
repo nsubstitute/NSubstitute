3f5807a14d56d3288e271675eb282be9c5b0f601
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2023-08-28 08:50:41 +0200

    Separated test cases

--- tests/NSubstitute.Acceptance.Specs/GenericArguments.cs
+++ tests/NSubstitute.Acceptance.Specs/GenericArguments.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
-using NSubstitute.Exceptions;
 using NUnit.Framework;
 
 namespace NSubstitute.Acceptance.Specs;
@@ -11,31 +9,49 @@ public class GenericArguments
 {
     public interface ISomethingWithGenerics
     {
-        void Log<TState>(int level, TState state);
+        void SomeAction<TState>(int level, TState state);
     }
 
     [Test]
-    public void Return_result_for_any_argument()
+    public void Any_matcher_works_with_AnyType()
     {
-        string argDoResult = null;
+        ISomethingWithGenerics something = Substitute.For<ISomethingWithGenerics>();
+
+        something.SomeAction(7, 3409);
+
+        something.Received().SomeAction(Arg.Any<int>(), Arg.Any<Arg.AnyType>());
+        something.Received().SomeAction(7, 3409);
+    }
+
+    [Test]
+    public void When_Do_works_with_AnyType()
+    {
         int? whenDoResult = null;
         bool whenDoCalled = false;
         ISomethingWithGenerics something = Substitute.For<ISomethingWithGenerics>();
-        something.Log(Arg.Any<int>(), Arg.Do<Arg.AnyType>(a => argDoResult = ">>" + ((int)a).ToString("P", CultureInfo.InvariantCulture)));
         something
-            .When(substitute => substitute.Log(Arg.Any<int>(), Arg.Any<Arg.AnyType>()))
+            .When(substitute => substitute.SomeAction(Arg.Any<int>(), Arg.Any<Arg.AnyType>()))
             .Do(info =>
             {
                 whenDoResult = info.ArgAt<int>(1);
                 whenDoCalled = true;
             });
 
-        something.Log(7, 3409);
+        something.SomeAction(7, 3409);
 
-        something.Received().Log(Arg.Any<int>(), Arg.Any<Arg.AnyType>());
-        something.Received().Log(7, 3409);
         Assert.That(whenDoCalled, Is.True);
-        Assert.That(argDoResult, Is.EqualTo(">>340,900.00 %"));
         Assert.That(whenDoResult, Is.EqualTo(3409));
+    }
+
+    [Test]
+    public void ArgDo_works_with_AnyType()
+    {
+        string argDoResult = null;
+        ISomethingWithGenerics something = Substitute.For<ISomethingWithGenerics>();
+        something.SomeAction(Arg.Any<int>(), Arg.Do<Arg.AnyType>(a => argDoResult = ">>" + ((int)a).ToString("P", CultureInfo.InvariantCulture)));
+
+        something.SomeAction(7, 3409);
+
+        Assert.That(argDoResult, Is.EqualTo(">>340,900.00 %"));
     }
 }
