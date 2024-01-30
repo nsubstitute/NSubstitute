5e34a59323201ebcaf4a33a5440c926cdbddf7b5
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2024-01-29 21:13:22 -0800

    Add a test for issue 771
    https://github.com/nsubstitute/NSubstitute/issues/771

---
+++ tests/NSubstitute.Acceptance.Specs/FieldReports/Issue771_NSubstituteLeakTests.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute.Exceptions;
+using NUnit.Framework;
+
+namespace NSubstitute.Acceptance.Specs.FieldReports;
+
+
+public class Issue771_NSubstituteLeakTests
+{
+    public class Foo;
+
+    [Test]
+    public void NSubstituteDoesNotLeak()
+    {
+        WeakReference<Foo> weakFoo;
+        {
+            var foo = new Foo();
+            weakFoo = new WeakReference<Foo>(foo);
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Assert.False(weakFoo.TryGetTarget(out _));
+    }
+}
+
+
