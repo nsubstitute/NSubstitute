4b9f00cc38947ccb382da9b5be71036a0348e5a6
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2024-01-30 20:57:14 -0800

    Align with the issue

--- tests/NSubstitute.Acceptance.Specs/FieldReports/Issue771_NSubstituteLeakTests.cs
+++ tests/NSubstitute.Acceptance.Specs/FieldReports/Issue771_NSubstituteLeakTests.cs
@@ -8,19 +8,36 @@ public class Issue771_NSubstituteLeakTests
 {
     public class Foo;
 
+    public interface IInterface
+    {
+        public void Method(Foo foo);
+    }
+    private readonly IInterface _interfaceMock = Substitute.For<IInterface>();
+
     [Test]
-    public void NSubstituteDoesNotLeak()
+    public async Task NSubstituteDoesNotLeak()
     {
         WeakReference<Foo> weakFoo;
         {
             var foo = new Foo();
             weakFoo = new WeakReference<Foo>(foo);
+            _interfaceMock.Method(foo);
+            _interfaceMock.ClearReceivedCalls();
         }
 
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
+        await GcCollect();
 
         Assert.False(weakFoo.TryGetTarget(out _));
+    }
+
+    private static async Task GcCollect()
+    {
+        for (var i = 0; i < 3; i++)
+        {
+            await Task.Yield();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
     }
 }
 
