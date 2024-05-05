66991e405c6095d6398e5057005be4231bec4ddf
David Tchepak <dave@davesquared.net>
2024-05-05 14:17:12 +1000

    Improve output for expected argument matchers
    - Add IDescribeSpecification to allow custom arg matchers to provide
      custom output for "expected to receive" entries.
    - Fallback to ToString when IDescribeSpecification not implemented.
    - Update code comment docs accordingly.
    - Improved support for custom matchers
    
    Closes #796.

--- src/NSubstitute/Core/Arguments/AnyArgumentMatcher.cs
+++ src/NSubstitute/Core/Arguments/AnyArgumentMatcher.cs
@@ -1,8 +1,14 @@
 namespace NSubstitute.Core.Arguments;
 
-public class AnyArgumentMatcher(Type typeArgMustBeCompatibleWith) : IArgumentMatcher
+public class AnyArgumentMatcher(Type typeArgMustBeCompatibleWith)
+    : IArgumentMatcher, IDescribeSpecification, IDescribeNonMatches
 {
     public override string ToString() => "any " + typeArgMustBeCompatibleWith.GetNonMangledTypeName();
 
     public bool IsSatisfiedBy(object? argument) => argument.IsCompatibleWith(typeArgMustBeCompatibleWith);
+
+    public string DescribeFor(object? argument) =>
+        argument?.GetType().GetNonMangledTypeName() ?? "<null>" + " is not a " + typeArgMustBeCompatibleWith.GetNonMangledTypeName();
+
+    public string DescribeSpecification() => ToString();
 }
