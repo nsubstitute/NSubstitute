aa3cd123654034bd8a889eb658e0605deeb503db
David Tchepak <dave@davesquared.net>
2024-05-05 22:33:47 +1000

    Apply review comments
    - use string.Empty for null value from IDescribeSpecification, rather than
      falling back to ToString(). This supports the contract that
      IDescribeSpecification will be used if implemented. Replacing
      null string.Empty with matches the documented
      bheaviour of IDescribeNonMatches.
    - updated IDescribeSpecification code docs.
    - removed GenericToNonGenericMatcherProxyWithDescribe `ToString` as it
      can use the GenericToNonGenericMatcherProxy superclass implementation.
    - update ArgumentSpecification to also support IDescribeSpecification
      for its matcher.
    - Replace linq with Array.ConvertAll rather than requiring an extra
      ToArray conversion.

--- src/NSubstitute/Core/Arguments/ArgumentMatcher.cs
+++ src/NSubstitute/Core/Arguments/ArgumentMatcher.cs
@@ -46,7 +46,9 @@ public static class ArgumentMatcher
         public bool IsSatisfiedBy(object? argument) => _matcher.IsSatisfiedBy((T?)argument!);
 
         public override string ToString() =>
-            (_matcher as IDescribeSpecification)?.DescribeSpecification() ?? _matcher.ToString() ?? "";
+            _matcher is IDescribeSpecification describe
+                ? describe.DescribeSpecification() ?? string.Empty
+                : _matcher.ToString() ?? string.Empty;
     }
 
     private class GenericToNonGenericMatcherProxyWithDescribe<T> : GenericToNonGenericMatcherProxy<T>, IDescribeNonMatches
@@ -57,9 +59,6 @@ public static class ArgumentMatcher
         }
 
         public string DescribeFor(object? argument) => ((IDescribeNonMatches)_matcher).DescribeFor(argument);
-
-        public override string ToString() =>
-            (_matcher as IDescribeSpecification)?.DescribeSpecification() ?? _matcher.ToString() ?? "";
     }
 
     private class DefaultValueContainer<T>
--- src/NSubstitute/Core/CallSpecification.cs
+++ src/NSubstitute/Core/CallSpecification.cs
@@ -123,9 +123,11 @@ public class CallSpecification : ICallSpecification
 
     public override string ToString()
     {
-        var argSpecsAsStrings = _argumentSpecifications.Select(x =>
-            (x as IDescribeSpecification)?.DescribeSpecification() ?? x.ToString() ?? string.Empty
-        ).ToArray();
+        var argSpecsAsStrings = Array.ConvertAll(_argumentSpecifications, x =>
+            x is IDescribeSpecification describe
+                ? describe.DescribeSpecification() ?? string.Empty
+                : x.ToString() ?? string.Empty
+        );
         return CallFormatter.Default.Format(GetMethodInfo(), argSpecsAsStrings);
     }
 
