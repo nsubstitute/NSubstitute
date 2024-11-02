72d146d6a22fade5086690eb5e8074b03fb359c4
Romfos <Romfos@tut.by>
2024-11-02 05:37:03 +0100

    Update target frameworks and other infrastructure changes (#831)
    * Update target frameworks
    * Simplify build system
    * Remove legacy obsolete api
    * Mark `CompatArg` as obsolete
    * Update github actions
    * Update changelogs
    * Suppress some warnings in tests
    * Update changelog

--- src/NSubstitute/Core/IArgumentSpecificationDequeue.cs
+++ src/NSubstitute/Core/IArgumentSpecificationDequeue.cs
@@ -1,12 +1,8 @@
-﻿using System.Reflection;
-using NSubstitute.Core.Arguments;
+﻿using NSubstitute.Core.Arguments;
 
 namespace NSubstitute.Core;
 
 public interface IArgumentSpecificationDequeue
 {
-    [Obsolete("This method is deprecated and will be removed in future versions of product.")]
-    IList<IArgumentSpecification> DequeueAllArgumentSpecificationsForMethod(MethodInfo methodInfo);
-
     IList<IArgumentSpecification> DequeueAllArgumentSpecificationsForMethod(int parametersCount);
 }
--- src/NSubstitute/Proxies/DelegateProxy/ProxiedDelegateTypeAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace NSubstitute.Proxies.DelegateProxy;
-
-[Obsolete("This class is deprecated and will be removed in future versions of the product.")]
-[AttributeUsage(AttributeTargets.Method)]
-public class ProxiedDelegateTypeAttribute(Type delegateType) : Attribute
-{
-    public Type DelegateType { get; } = delegateType;
-}
