334894f73a671420880d39194d7a47b0879575d4
Romfos <Romfos@tut.by>
2024-12-04 13:49:05 +0100

    Cleanup build project

--- build/build.fs
+++ build/build.fs
@@ -8,9 +8,6 @@ open Fake.IO.FileSystemOperators
 
 open ExtractDocs
 
-let target = Target.create
-let description = Target.description
-
 module FileReaderWriter =
     let Read file = File.ReadAllText(file)
     let Write file (text: string) = File.WriteAllText(file, text)
@@ -36,60 +33,39 @@ module ExamplesToCode =
             for file in files do
                 ConvertFile file targetDir
 
-type BuildVersion = { assembly: string; file: string; info: string; package: string }
 
 let root = __SOURCE_DIRECTORY__ </> ".." |> Path.getFullName
 
 let configuration = Environment.environVarOrDefault "configuration" "Debug"
 
 let output = root </> "bin" </> configuration
-let solution = (root </> "NSubstitute.sln")
-
-let initTargets() =
-    Target.description("Extract, build and test code from documentation.")
-    Target.create "TestCodeFromDocs" <| fun _ ->
-        let outputCodePath = output </> "CodeFromDocs"
-        Directory.create outputCodePath
-        // generate samples from docs
-        ExamplesToCode.Convert [ root </> "docs/"; root </> "docs/docs"; root ] outputCodePath
-        // compile code samples
-        let csproj = """
-            <Project Sdk="Microsoft.NET.Sdk">
-            <PropertyGroup>
-                <TargetFrameworks>net8.0;net462</TargetFrameworks>
-                <LangVersion>latest</LangVersion>
-            </PropertyGroup>
-            <ItemGroup>
-                <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.12.0" />
-                <PackageReference Include="NUnit" Version="3.14.0" />
-                <PackageReference Include="NUnit3TestAdapter" Version="4.6.0" />
-            </ItemGroup>
-            <ItemGroup>
-                <ProjectReference Include="..\..\..\src\NSubstitute\NSubstitute.csproj" />
-            </ItemGroup>
-        </Project>
-        """
-        let projPath = outputCodePath </> "Docs.csproj"
-        FileReaderWriter.Write projPath csproj
-        DotNet.restore (fun p -> p) projPath
-        DotNet.build (fun p -> p) projPath
-        DotNet.test (fun p -> p) projPath
-
-    Target.description("List targets, similar to `rake -T`. For more details, run `--listTargets` instead.")
-    Target.create "-T" <| fun _ ->
-        printfn "Optional config options:"
-        printfn "  configuration=Debug|Release"
-        printfn "  benchmark=*|<benchmark name>  (only for Benchmarks target in Release mode)"
-        printfn ""
-        Target.listAvailable()
 
 [<EntryPoint>]
 let main argv =
-    argv
-    |> Array.toList
-    |> Context.FakeExecutionContext.Create false "build.fsx"
-    |> Context.RuntimeContext.Fake
-    |> Context.setExecutionContext
-    initTargets()
-    Target.runOrDefaultWithArguments "TestCodeFromDocs"
-    0 
+    let outputCodePath = output </> "CodeFromDocs"
+    Directory.create outputCodePath
+    // generate samples from docs
+    ExamplesToCode.Convert [ root </> "docs/"; root </> "docs/docs"; root ] outputCodePath
+    // compile code samples
+    let csproj = """
+        <Project Sdk="Microsoft.NET.Sdk">
+        <PropertyGroup>
+            <TargetFrameworks>net8.0;net462</TargetFrameworks>
+            <LangVersion>latest</LangVersion>
+        </PropertyGroup>
+        <ItemGroup>
+            <PackageReference Include="Microsoft.NET.Test.Sdk" Version="17.12.0" />
+            <PackageReference Include="NUnit" Version="3.14.0" />
+            <PackageReference Include="NUnit3TestAdapter" Version="4.6.0" />
+        </ItemGroup>
+        <ItemGroup>
+            <ProjectReference Include="..\..\..\src\NSubstitute\NSubstitute.csproj" />
+        </ItemGroup>
+    </Project>
+    """
+    let projPath = outputCodePath </> "Docs.csproj"
+    FileReaderWriter.Write projPath csproj
+    DotNet.restore (fun p -> p) projPath
+    DotNet.build (fun p -> p) projPath
+    DotNet.test (fun p -> p) projPath
+    0
