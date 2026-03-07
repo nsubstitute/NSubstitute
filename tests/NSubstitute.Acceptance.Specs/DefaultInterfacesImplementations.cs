1e8c99f9f43df51c8ad92ad2947076eb11a92033
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2026-03-07 21:00:22 +0100

    Added DefaultInterfacesImplementations

---
+++ tests/NSubstitute.Acceptance.Specs/DefaultInterfacesImplementations.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSubstitute.Acceptance.Specs;
+
+#if NET8_0_OR_GREATER
+
+[TestFixture]
+public class DefaultInterfacesImplementations
+{
+    [Test]
+    public void DefaultInterface_SetupReturns_UsesReturns()
+    {
+        // Arrange
+        var for1 = Substitute.For<IMyInterface>();
+        for1.GetText().Returns("new text");
+
+        // Act
+        var result = for1.GetText();
+
+        // Assert
+        Assert.That(result, Is.EqualTo("new text"));
+    }
+
+    public interface IMyInterface
+    {
+        string GetText()
+        {
+            return "default text";
+        }
+    }
+}
+#endif
